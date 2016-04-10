be65f09f9e4b84bde9c03a7ee1ad487046745b26
Chris Pacia <ctpacia@gmail.com>
2016-04-10 13:36:44 -0400

    Refactor add and request

--- ipfs/add.go
+++ ipfs/add.go
@@ -5,11 +5,7 @@ import (
 	"errors"
 	"github.com/ipfs/go-ipfs/commands"
 	"github.com/ipfs/go-ipfs/core/coreunix"
-	"github.com/op/go-logging"
-	"gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
 )
-
-var log = logging.MustGetLogger("ipfs")
 
 var addErr = errors.New(`Add directory failed`)
 
@@ -17,18 +13,11 @@ var addErr = errors.New(`Add directory failed`)
 func AddDirectory(ctx commands.Context, fpath string) (string, error) {
 	_, root := path.Split(fpath)
 	args := []string{"add", "-r", fpath}
-	req, cmd, err := NewRequest(args)
+	req, cmd, err := NewRequest(ctx, args)
 	if err != nil {
 		return "", err
 	}
 	res := commands.NewResponse(req)
-	cctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	req.SetRootContext(cctx)
-	req.SetInvocContext(ctx)
-	if err != nil {
-		return "", err
-	}
 	cmd.PreRun(req)
 	cmd.Run(req, res)
 	var rootHash string
--- ipfs/request.go
+++ ipfs/request.go
@@ -1,11 +1,14 @@
 package ipfs
 
 import (
+	"github.com/op/go-logging"
 	cmds "github.com/ipfs/go-ipfs/commands"
 	commands "github.com/ipfs/go-ipfs/core/commands"
 	cli "github.com/ipfs/go-ipfs/commands/cli"
+	"gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
 )
 
+var log = logging.MustGetLogger("ipfs")
 
 var Root = &cmds.Command{
 	Options:  commands.Root.Options,
@@ -18,7 +21,7 @@ var localCommands = map[string]*cmds.Command{
 	"commands": commandsClientCmd,
 }
 
-func NewRequest(args []string) (cmds.Request, *cmds.Command, error) {
+func NewRequest(ctx cmds.Context, args []string) (cmds.Request, *cmds.Command, error) {
 	Root.Subcommands = localCommands
 	for k, v := range commands.Root.Subcommands {
 		if _, found := Root.Subcommands[k]; !found {
@@ -26,5 +29,12 @@ func NewRequest(args []string) (cmds.Request, *cmds.Command, error) {
 		}
 	}
 	req, cmd, _, err := cli.Parse(args, nil, Root)
+	cctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	rerr := req.SetRootContext(cctx)
+	if rerr != nil {
+		return nil, nil, rerr
+	}
+	req.SetInvocContext(ctx)
 	return req, cmd, err
 }
