afb3cd44e99971bf420b57e83af7f6434c8384e3
Chris Pacia <ctpacia@gmail.com>
2016-04-17 22:17:38 -0400

    Add contracts proto

---
+++ ipfs/pointers.go
@@ -0,0 +1,20 @@
+package ipfs
+
+import (
+	multihash "gx/ipfs/QmYf7ng2hG5XBtJA3tN34DQ2GUN5HNksEw1rLDkmr6vGku/go-multihash"
+	peer "gx/ipfs/QmccGfZs3rzku8Bv6sTPH3bMUKD1EVod8srgRjt5csdmva/go-libp2p/p2p/peer"
+)
+
+var MAGIC []byte = make([]byte, 32)
+
+func getMagicID() (peer.ID, error){
+	h, err := multihash.Encode(MAGIC, multihash.SHA2_256)
+	if err != nil {
+		return "", err
+	}
+	id, err := peer.IDFromBytes(h)
+	if err != nil {
+		return "", err
+	}
+	return id, nil
+}
