fc1b6cf4dbd616617742e6897aaf546588bc8629
Brian Hoffman <brianchoffman@gmail.com>
2016-08-03 15:19:12 -0400

    Fix

--- test_compile.sh
+++ test_compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pwd
 go test -coverprofile=api.cover.out ./api/*_test.go
 go test -coverprofile=bitcoin.cover.out ./bitcoin/*_test.go
 go test -coverprofile=core.cover.out ./core/*_test.go
@@ -8,7 +9,7 @@ go test -coverprofile=net.cover.out ./net/*_test.go
 go test -coverprofile=netservice.cover.out ./net/service/*_test.go
 go test -coverprofile=pb.cover.out =./pb/*_test.go
 go test -coverprofile=repo.cover.out =./repo/*_test.go
-go test -coverprofile=repodb.cover.out ./repo/db/*_test.go
+go test -coverprofile=repodb.cover.out ./repo/db
 go test -coverprofile=storage.cover.out ./storage/*_test.go
 go test -coverprofile=dropbox.cover.out ./storage/dropbox/*_test.go
 go test -coverprofile=selfhosted.cover.out ./storage/selfhosted/*_test.go
