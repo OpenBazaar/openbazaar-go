bb5d769d09d54619f293ed0dfe6d38bd0af6ccfe
Brian Hoffman <brianchoffman@gmail.com>
2016-08-03 11:53:34 -0400

    Only test _test.go files

--- test_compile.sh
+++ test_compile.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-go test -coverprofile=api.cover.out ./api/*
-go test -coverprofile=bitcoin.cover.out ./bitcoin/*
-go test -coverprofile=core.cover.out ./core/*
-go test -coverprofile=ipfs.cover.out ./ipfs/*
-go test -coverprofile=net.cover.out ./net/*
-go test -coverprofile=netservice.cover.out ./net/service/*
-go test -coverprofile=pb.cover.out =./pb/*
-go test -coverprofile=repo.cover.out =./repo/*
-go test -coverprofile=repodb.cover.out ./repo/db/*
-go test -coverprofile=storage.cover.out ./storage/*
-go test -coverprofile=dropbox.cover.out ./storage/dropbox/*
-go test -coverprofile=selfhosted.cover.out ./storage/selfhosted/*
+go test -coverprofile=api.cover.out ./api/*_test.go
+go test -coverprofile=bitcoin.cover.out ./bitcoin/*_test.go
+go test -coverprofile=core.cover.out ./core/*_test.go
+go test -coverprofile=ipfs.cover.out ./ipfs/*_test.go
+go test -coverprofile=net.cover.out ./net/*_test.go
+go test -coverprofile=netservice.cover.out ./net/service/*_test.go
+go test -coverprofile=pb.cover.out =./pb/*_test.go
+go test -coverprofile=repo.cover.out =./repo/*_test.go
+go test -coverprofile=repodb.cover.out ./repo/db/*_test.go
+go test -coverprofile=storage.cover.out ./storage/*_test.go
+go test -coverprofile=dropbox.cover.out ./storage/dropbox/*_test.go
+go test -coverprofile=selfhosted.cover.out ./storage/selfhosted/*_test.go
 echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
 awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
 rm -rf *.cover.out
