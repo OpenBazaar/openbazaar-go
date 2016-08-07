214e55fc598490f61dbf3f6b58f4d1d5d3b8a208
David Albrecht <albrecht@uscreen.de>
2016-08-07 19:18:22 +0200

    adds tests to three functions in the repo package
    these are:
    GetAPIHeaders
    GetAPIEnabled
    GetAPICORS

--- repo/config_test.go
+++ repo/config_test.go
@@ -1,10 +1,12 @@
 package repo
 
 import (
+	"reflect"
 	"testing"
 )
 
 const testConfigPath = "testdata/config"
+const nonexistentTestConfigPath = "testdata/nonexistent"
 
 func TestGetAPIUsernameAndPw(t *testing.T) {
 	username, pw, err := GetAPIUsernameAndPw(testConfigPath)
@@ -18,7 +20,7 @@ func TestGetAPIUsernameAndPw(t *testing.T) {
 		t.Error("GetAPIUsernameAndPw threw an unexpected error")
 	}
 
-	username, pw, err = GetAPIUsernameAndPw("testdata/nonexistent")
+	username, pw, err = GetAPIUsernameAndPw(nonexistentTestConfigPath)
 	if username != "" {
 		t.Error("Expected empty string, got ", username)
 	}
@@ -27,5 +29,59 @@ func TestGetAPIUsernameAndPw(t *testing.T) {
 	}
 	if err == nil {
 		t.Error("GetAPIUsernameAndPw didn`t throw an error")
+	}
+}
+
+func TestGetAPIHeaders(t *testing.T) {
+	headers, err := GetAPIHeaders(testConfigPath)
+	if reflect.ValueOf(headers).Kind() != reflect.Map {
+		t.Error("headers is not a map")
+	}
+	if err != nil {
+		t.Error("GetAPIHeaders threw an unexpected error")
+	}
+
+	headers, err = GetAPIHeaders(nonexistentTestConfigPath)
+	if reflect.ValueOf(headers).Kind() != reflect.Map {
+		t.Error("headers is not a map")
+	}
+	if err == nil {
+		t.Error("GetAPIHeaders didn`t throw an error")
+	}
+}
+
+func TestGetAPIEnabled(t *testing.T) {
+	enabled, err := GetAPIEnabled(testConfigPath)
+	if enabled != true {
+		t.Error("enabled is not true")
+	}
+	if err != nil {
+		t.Error("GetAPIEnabled threw an unexpected error")
+	}
+
+	enabled, err = GetAPIEnabled(nonexistentTestConfigPath)
+	if enabled != false {
+		t.Error("enabled is not false when path to config file is nonexistent")
+	}
+	if err == nil {
+		t.Error("GetAPIEnabled didn`t throw an error")
+	}
+}
+
+func TestGetAPICORS(t *testing.T) {
+	cors, err := GetAPICORS(testConfigPath)
+	if cors != true {
+		t.Error("cors is not true")
+	}
+	if err != nil {
+		t.Error("GetAPICORS threw an unexpected error")
+	}
+
+	cors, err = GetAPICORS(nonexistentTestConfigPath)
+	if cors != false {
+		t.Error("cors is not false when path to config file is nonexistent")
+	}
+	if err == nil {
+		t.Error("GetAPICORS didn`t throw an error")
 	}
 }
