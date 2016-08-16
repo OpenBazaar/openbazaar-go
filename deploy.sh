f2aeed417980a797ad20534592cd309764cb1300
Brian Hoffman <brianchoffman@gmail.com>
2016-08-16 16:12:57 -0400

    Hash and Sign Binaries

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,30 @@
 if [ ! -z "$TRAVIS_TAG" ] &&
     [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "This will deploy!"
+
+  # Cross-compile for all platforms
   export CGO_ENABLED=1
   docker pull karalabe/xgo-latest
   go get github.com/karalabe/xgo
   mkdir dist/ && cd dist/
   xgo --targets=windows/386,windows/amd64,darwin/amd64,linux/386,linux/amd64,linux/arm ../
   chmod +x *
+
+  # Calculate SHA512 hashes
+  sha512sum * > sha512_checksums.txt
+
+  # Load signing key
+  cp ../.travis/sign.key.gpg  /tmp
+  gpg --yes --batch --passphrase=$GPG_PASS /tmp/sign.key.gpg
+  gpg --allow-secret-key-import --import /tmp/sign.key
+  rm /tmp/sign.key
+
+  # Sign binaries
+  for f in *
+  do
+  	gpg --armor --output $f.asc --detach-sig --passphrase=$GPG_PASS --default-key $GPG_KEYID $f
+  done
+
   ghr --username OpenBazaar -t $GITHUB_TOKEN --replace --prerelease --debug $TRAVIS_TAG .
 else
   echo "This will not deploy!"
