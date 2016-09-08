2e4ea71d63378167c2695e5ec15be6d0cfa411bd
Chris Pacia <chris@ob1.io>
2016-09-08 01:49:09 -0400

    Update test_compile.sh

--- test_compile.sh
+++ test_compile.sh
@@ -2,12 +2,16 @@
 
 pwd
 go test -coverprofile=api.cover.out ./api
+go test -coverprofile=api.cover.out ./api/notifications
 go test -coverprofile=bitcoin.cover.out ./bitcoin
 go test -coverprofile=bitcoin.cover.out ./bitcoin/exchange
+go test -coverprofile=bitcoin.cover.out ./bitcoin/listeners
 go test -coverprofile=core.cover.out ./core
 go test -coverprofile=ipfs.cover.out ./ipfs
 go test -coverprofile=net.cover.out ./net
 go test -coverprofile=netservice.cover.out ./net/service
+go test -coverprofile=netservice.cover.out ./net/repointer
+go test -coverprofile=netservice.cover.out ./net/retriever
 go test -coverprofile=repo.cover.out ./repo
 go test -coverprofile=repodb.cover.out ./repo/db
 go test -coverprofile=storage.cover.out ./storage
