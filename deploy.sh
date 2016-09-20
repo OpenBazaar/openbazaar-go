207e0189526e6d3e1dfe56be7e297528a939814b
Chris Pacia <chris@ob1.io>
2016-09-20 18:14:50 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,9 @@ if [ ! -z "$TRAVIS_TAG" ] &&
   rm /tmp/sign.key.gpg
 
   # Sign hash file
-  gpg --armor --output sha512_checksums.asc --detach-sig --passphrase=$GPG_PASS --default-key $GPG_KEYID sha512_checksums.txt
+  gpg --clearsign --digest-algo SHA256 --armor --output sha512_checksums.asc --passphrase=$GPG_PASS --default-key $GPG_KEYID sha512_checksums.txt
+
+  rm sha512_checksums.txt
 
   # Upload to GitHub Release page
   ghr --username OpenBazaar -t $GITHUB_TOKEN --replace --prerelease --debug $TRAVIS_TAG .
