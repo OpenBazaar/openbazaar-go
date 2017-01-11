5dc20637907e05c4ab53f39313a499935c8dfb6b
Tyler Smith <tylersmith.me@gmail.com>
2017-01-10 21:01:51 -0800

    REFACTOR: Extract compilation to its own sh script.

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,7 @@ if [ ! -z "$TRAVIS_TAG" ] &&
   echo "This will deploy!"
 
   # Cross-compile for all platforms
-  export CGO_ENABLED=1
-  docker pull karalabe/xgo-latest
-  go get github.com/karalabe/xgo
-  mkdir dist/ && cd dist/
-  xgo --targets=windows/386,windows/amd64,darwin/amd64,linux/386,linux/amd64,linux/arm ../
-  chmod +x *
+  ./build.sh
 
   # Calculate SHA512 hashes
   sha512sum * > sha512_checksums.txt
