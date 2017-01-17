9c085d897ca489c90149b5fede5679263a1f1db7
Chris Pacia <chris@ob1.io>
2017-01-17 18:59:06 -0500

    Use correct repo path for encrypt and decrypt database functions

--- repo/db/encryption.go
+++ repo/db/encryption.go
@@ -12,6 +12,7 @@ import (
 	lockfile "github.com/ipfs/go-ipfs/repo/fsrepo/lock"
 	"github.com/mitchellh/go-homedir"
 	"golang.org/x/crypto/ssh/terminal"
+	"runtime"
 )
 
 // FIXME: the encrypt and decrypt functions here should probably be added to the DB interface
@@ -21,39 +22,46 @@ import (
 func Encrypt() error {
 	reader := bufio.NewReader(os.Stdin)
 	var repoPath string
+	var dbPath string
 	var filename string
 	var testnet bool
+	var err error
 	for {
 		fmt.Print("Encrypt the mainnet or testnet db?: ")
 		resp, _ := reader.ReadString('\n')
 		if strings.Contains(strings.ToLower(resp), "mainnet") {
-			rPath := "~/.openbazaar2.0"
+			repoPath, err = getRepoPath(false)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
 			filename = "mainnet.db"
-			testnet = false
-			expPath, _ := homedir.Expand(filepath.Clean(rPath))
-			repoPath = expPath
+			dbPath = path.Join(repoPath, "datastore", filename)
 			repoLockFile := filepath.Join(repoPath, lockfile.LockFile)
 			if _, err := os.Stat(repoLockFile); !os.IsNotExist(err) {
 				fmt.Println("Cannot encrypt while the daemon is running.")
 				return nil
 			}
-			if _, err := os.Stat(expPath); os.IsNotExist(err) {
+			if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 				fmt.Println("Database does not exist. You may need to run the node at least once to initialize it.")
 				return nil
 			}
 			break
 		} else if strings.Contains(strings.ToLower(resp), "testnet") {
-			rPath := "~/.openbazaar2.0-testnet"
-			filename = "testnet.db"
+			repoPath, err = getRepoPath(true)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
 			testnet = true
-			expPath, _ := homedir.Expand(filepath.Clean(rPath))
-			repoPath = expPath
+			filename = "testnet.db"
+			dbPath = path.Join(repoPath, "datastore", filename)
 			repoLockFile := filepath.Join(repoPath, lockfile.LockFile)
 			if _, err := os.Stat(repoLockFile); !os.IsNotExist(err) {
 				fmt.Println("Cannot encrypt while the daemon is running.")
 				return nil
 			}
-			if _, err := os.Stat(expPath); os.IsNotExist(err) {
+			if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 				fmt.Println("Database does not exist. You may need to run the daemon at least once to initialize it.")
 				return nil
 			}
@@ -126,40 +134,48 @@ func Encrypt() error {
 // Create a temp database, read the encrypted database into it then replace the encrypted database
 func Decrypt() error {
 	reader := bufio.NewReader(os.Stdin)
+
 	var repoPath string
+	var dbPath string
 	var filename string
 	var testnet bool
+	var err error
 	for {
 		fmt.Print("Decrypt the mainnet or testnet db?: ")
 		resp, _ := reader.ReadString('\n')
-		if strings.ToLower(resp) == "mainnet\n" {
-			rPath := "~/.openbazaar2.0"
+		if strings.Contains(strings.ToLower(resp), "mainnet") {
+			repoPath, err = getRepoPath(false)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
 			filename = "mainnet.db"
-			testnet = false
-			expPath, _ := homedir.Expand(filepath.Clean(rPath))
-			repoPath = expPath
+			dbPath = path.Join(repoPath, "datastore", filename)
 			repoLockFile := filepath.Join(repoPath, lockfile.LockFile)
 			if _, err := os.Stat(repoLockFile); !os.IsNotExist(err) {
 				fmt.Println("Cannot decrypt while the daemon is running.")
 				return nil
 			}
-			if _, err := os.Stat(expPath); os.IsNotExist(err) {
+			if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 				fmt.Println("Database does not exist. You may need to run the daemon at least once to initialize it.")
 				return nil
 			}
 			break
-		} else if strings.ToLower(resp) == "testnet\n" {
-			rPath := "~/.openbazaar2.0-testnet"
-			filename = "testnet.db"
+		} else if strings.Contains(strings.ToLower(resp), "testnet") {
+			repoPath, err = getRepoPath(true)
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
 			testnet = true
-			expPath, _ := homedir.Expand(filepath.Clean(rPath))
-			repoPath = expPath
+			filename = "testnet.db"
+			dbPath = path.Join(repoPath, "datastore", filename)
 			repoLockFile := filepath.Join(repoPath, lockfile.LockFile)
 			if _, err := os.Stat(repoLockFile); !os.IsNotExist(err) {
 				fmt.Println("Cannot decrypt while the daemon is running.")
 				return nil
 			}
-			if _, err := os.Stat(expPath); os.IsNotExist(err) {
+			if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 				fmt.Println("Database does not exist. You may need to run the node at least once to initialize it.")
 				return nil
 			}
@@ -199,4 +215,32 @@ func Decrypt() error {
 	os.RemoveAll(path.Join(repoPath, "tmp"))
 	fmt.Println("Success!")
 	return nil
+}
+
+func getRepoPath(isTestnet bool) (string, error) {
+	// Set default base path and directory name
+	path := "~"
+	directoryName := "OpenBazaar2.0"
+
+	// Override OS-specific names
+	switch runtime.GOOS {
+	case "linux":
+		directoryName = ".openbazaar2.0"
+	case "darwin":
+		path = "~/Library/Application Support"
+	}
+
+	// Append testnet flag if on testnet
+	if isTestnet {
+		directoryName += "-testnet"
+	}
+
+	// Join the path and directory name, then expand the home path
+	fullPath, err := homedir.Expand(filepath.Join(path, directoryName))
+	if err != nil {
+		return "", err
+	}
+
+	// Return the shortest lexical representation of the path
+	return filepath.Clean(fullPath), nil
 }
