da390a774379505c35c19f90cae0497b3a848b04
Tyler Smith <tylersmith.me@gmail.com>
2017-01-25 12:34:01 -0800

    BUGFIX: Fix build artifacts.

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ TARGETS=${1:-windows/386,windows/amd64,darwin/amd64,linux/386,linux/amd64,linux/
 export CGO_ENABLED=1
 docker pull karalabe/xgo-latest
 go get github.com/karalabe/xgo
-mkdir dist
-(cd dist/ && xgo --targets=$TARGETS ../)
-(chmod +x ./dist/*)
+mkdir dist && cd dist/
+xgo --targets=$TARGETS ../
+chmod +x *
