f795d6af4841b51809bc405d66c578b8f9010091
Josh Jeffryes <jjeffryes@gmail.com>
2017-02-09 14:56:25 -0500

    Adjust spacing.

--- core/listings.go
+++ core/listings.go
@@ -890,8 +890,8 @@ func validateListing(listing *pb.Listing) (err error) {
 			return fmt.Errorf("Coupon title length must be less than the max of %d", SentenceMaxCharacters)
 		}
 		if len(coupon.GetDiscountCode()) > SentenceMaxCharacters {
-            return fmt.Errorf("Coupon code length must be less than the max of %d", SentenceMaxCharacters)
-        }
+			return fmt.Errorf("Coupon code length must be less than the max of %d", SentenceMaxCharacters)
+		}
 		if coupon.GetPercentDiscount() > 100 {
 			return errors.New("Percent discount cannot be over 100 percent")
 		}
