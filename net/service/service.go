a101ca43736fe7cb3c5409af97c761cd632a804c
Jack Kleeman <jackkleeman@gmail.com>
2017-03-15 12:07:22 +0000

    add a messagesender which can reuse streams, and update the stream handler to expect many messages over the same stream

--- net/service/service.go
+++ net/service/service.go
@@ -9,6 +9,7 @@ import (
 	protocol "gx/ipfs/QmZNkThpqfVXs9GNbexPrfBbXSLNYeKrE7jwFM2oqHbyqN/go-libp2p-protocol"
 	ps "gx/ipfs/QmeXj9VAjmYQZxpmVz7VzccbJrpmr8qkCDSjfVNsPTWTYU/go-libp2p-peerstore"
 	peer "gx/ipfs/QmfMmLGoKzCHDN7cGgk64PJr4iipzidDRME8HABSJqvmhC/go-libp2p-peer"
+	"sync"
 
 	"github.com/OpenBazaar/openbazaar-go/core"
 	"github.com/OpenBazaar/openbazaar-go/pb"
@@ -31,6 +32,8 @@ type OpenBazaarService struct {
 	broadcast chan interface{}
 	datastore repo.Datastore
 	node      *core.OpenBazaarNode
+	sender    map[peer.ID]*messageSender
+	senderlk  sync.Mutex
 }
 
 func New(node *core.OpenBazaarNode, ctx commands.Context, datastore repo.Datastore) *OpenBazaarService {
@@ -59,71 +62,67 @@ func (service *OpenBazaarService) handleNewMessage(s inet.Stream) {
 	r := ggio.NewDelimitedReader(cr, inet.MessageSizeMax)
 	w := ggio.NewDelimitedWriter(cw)
 	mPeer := s.Conn().RemotePeer()
-
 	// Check if banned
 	if service.node.BanManager.IsBanned(mPeer) {
 		return
 	}
 
-	// Receive msg
+	// ensure the message sender for this peer is updated with this stream, so we reply over it
+	service.messageSenderForPeer(mPeer, &s)
 	defer s.Close()
-	pmes := new(pb.Message)
-	if err := r.ReadMsg(pmes); err != nil {
-		log.Errorf("Error unmarshaling data: %s", err)
-	}
+	i := 0 // REMOVE
+	for {
+		log.Info("inbound stream reuse count:", i)
+		i++
+		// Receive msg
+		pmes := new(pb.Message)
+		if err := r.ReadMsg(pmes); err != nil {
+			log.Errorf("Error unmarshaling data: %s", err)
+			return
+		}
 
-	// Get handler for this msg type
-	handler := service.HandlerForMsgType(pmes.MessageType)
-	if handler == nil {
-		log.Debug("Got back nil handler from handlerForMsgType")
-		return
-	}
+		// Get handler for this msg type
+		handler := service.HandlerForMsgType(pmes.MessageType)
+		if handler == nil {
+			log.Debug("Got back nil handler from handlerForMsgType")
+			return
+		}
 
-	// Dispatch handler
-	rpmes, err := handler(mPeer, pmes, nil)
-	if err != nil {
-		log.Debugf("handle message error: %s", err)
-		return
-	}
+		// Dispatch handler
+		rpmes, err := handler(mPeer, pmes, nil)
+		if err != nil {
+			log.Debugf("handle message error: %s", err)
+			return
+		}
 
-	// If nil response, return it before serializing
-	if rpmes == nil {
-		return
-	}
+		// If nil response, return it before serializing
+		if rpmes == nil {
+			continue
+		}
 
-	// Send out response msg
-	if err := w.WriteMsg(rpmes); err != nil {
-		log.Debugf("send response error: %s", err)
-		return
+		// Send out response msg
+		if err := w.WriteMsg(rpmes); err != nil {
+			log.Debugf("send response error: %s", err)
+			return
+		}
 	}
 }
 
 func (service *OpenBazaarService) SendRequest(ctx context.Context, p peer.ID, pmes *pb.Message) (*pb.Message, error) {
 	log.Debugf("Sending %s request to %s", pmes.MessageType.String(), p.Pretty())
-	s, err := service.host.NewStream(ctx, p, ProtocolOpenBazaar)
+	ms := service.messageSenderForPeer(p, nil)
+
+	rpmes, err := ms.SendRequest(ctx, pmes)
 	if err != nil {
-		return nil, err
-	}
-	defer s.Close()
-
-	cr := ctxio.NewReader(ctx, s) // Ok to use. We defer close stream in this func.
-	cw := ctxio.NewWriter(ctx, s) // Ok to use. We defer close stream in this func.
-	r := ggio.NewDelimitedReader(cr, inet.MessageSizeMax)
-	w := ggio.NewDelimitedWriter(cw)
-
-	if err := w.WriteMsg(pmes); err != nil {
-		return nil, err
-	}
-
-	rpmes := new(pb.Message)
-	if err := r.ReadMsg(rpmes); err != nil {
 		log.Debugf("No response from %s", p.Pretty())
 		return nil, err
 	}
+
 	if rpmes == nil {
 		log.Debugf("No response from %s", p.Pretty())
 		return nil, errors.New("no response from peer")
 	}
+
 	log.Debugf("Received response from %s", p.Pretty())
 
 	return rpmes, nil
@@ -131,16 +130,9 @@ func (service *OpenBazaarService) SendRequest(ctx context.Context, p peer.ID, pm
 
 func (service *OpenBazaarService) SendMessage(ctx context.Context, p peer.ID, pmes *pb.Message) error {
 	log.Debugf("Sending %s message to %s", pmes.MessageType.String(), p.Pretty())
-	s, err := service.host.NewStream(ctx, p, ProtocolOpenBazaar)
-	if err != nil {
-		return err
-	}
-	defer s.Close()
+	ms := service.messageSenderForPeer(p, nil)
 
-	cw := ctxio.NewWriter(ctx, s) // Ok to use. We defer close stream in this func.
-	w := ggio.NewDelimitedWriter(cw)
-
-	if err := w.WriteMsg(pmes); err != nil {
+	if err := ms.SendMessage(ctx, pmes); err != nil {
 		return err
 	}
 	return nil
