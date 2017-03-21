868a475910fee96494d2752d9a3034c149743a8a
Tyler Smith <tylersmith.me@gmail.com>
2017-03-21 04:14:54 -0700

    REFACTOR: Update to latest pb/ipfs code, remove fake handles, and add images.

--- dummy/listing.go
+++ dummy/listing.go
@@ -3,6 +3,7 @@ package main
 import (
 	"math/rand"
 	"strconv"
+	"time"
 
 	"github.com/OpenBazaar/openbazaar-go/pb"
 	"github.com/golang/protobuf/ptypes/timestamp"
@@ -23,10 +24,10 @@ var categories = []string{
 	"Other",
 }
 
-func newRandomListing() *pb.ListingReqApi {
+func newRandomListing(randomImages chan (*randomImage)) *pb.Listing {
 	title := fake.ProductName()
 	slug := slugify(title)
-	sku := slug + "-" + fake.Digits()
+	// sku := slug + "-" + fake.Digits()
 
 	tags := make([]string, rand.Intn(9)+1)
 	tags[0] = fake.Word()
@@ -36,11 +37,13 @@ func newRandomListing() *pb.ListingReqApi {
 
 	// Create a random amount of options with random amounts of varients
 	options := make([]*pb.Listing_Item_Option, rand.Intn(4)+2)
+	variantCount := rand.Intn(2) + 2
+
 	for i := 0; i < len(options); i++ {
 		options[i] = &pb.Listing_Item_Option{
 			Name:        fake.ProductName(),
 			Description: fake.Sentence(),
-			Variants:    make([]*pb.Listing_Item_Option_Variants, rand.Intn(2)+2),
+			Variants:    make([]*pb.Listing_Item_Option_Variant, variantCount),
 		}
 
 		// Ensure description is <= 70
@@ -48,23 +51,31 @@ func newRandomListing() *pb.ListingReqApi {
 			options[i].Description = options[i].Description[:70]
 		}
 
+		// Set variant name and image
 		for j := 0; j < len(options[i].Variants); j++ {
-			options[i].Variants[j] = &pb.Listing_Item_Option_Variants{
-				Name: title + " " + fake.ProductName(),
-				Image: &pb.Listing_Item_Image{
-					Filename: "example.jpg",
-					Original: "QmdfiTnhj1oqiCDmhxu1gdgW6ZqtR7D6ZE7j7CqWUHgKJ8",
-					Tiny:     "QmbKPEBbzVwax8rnrdxLepfmNkdTqnw2RSfJE19iax3fLK",
-					Small:    "QmQ77aAsYjs1rxcp7xZ5qUki1RGDGjQ99cok3ynUMF8Sc5",
-					Medium:   "QmVoh493xbSaKYV9yLtEapaGQ7J31gdCiDQHGSd86PNo8B",
-					Large:    "QmUWuTZhjUuY8VYWVZrcodsrsbQvckSZwTRxmJ3Avhnw1y",
-				},
-				PriceModifier: int64(rand.Intn(50)),
+			// Get fake name <= 40 characters
+			name := fake.Word()
+			if len(name) > 40 {
+				name = name[:40]
 			}
 
-			// Ensure name is <= 40 chars
-			if len(options[i].Variants[j].Name) > 40 {
-				options[i].Variants[j].Name = options[i].Variants[j].Name[:40]
+			// Create variant
+			options[i].Variants[j] = &pb.Listing_Item_Option_Variant{Name: name}
+
+			// Get fake image
+			timeout := time.NewTimer(2 * time.Second)
+			select {
+			case <-timeout.C:
+				break
+			case image := <-randomImages:
+				options[i].Variants[j].Image = &pb.Listing_Item_Image{
+					Filename: image.filename,
+					Tiny:     image.Tiny,
+					Small:    image.Small,
+					Medium:   image.Medium,
+					Large:    image.Large,
+					Original: image.Original,
+				}
 			}
 		}
 	}
@@ -74,67 +85,80 @@ func newRandomListing() *pb.ListingReqApi {
 		countries[i] = pb.CountryCode(rand.Intn(255))
 	}
 
-	return &pb.ListingReqApi{
-		Listing: &pb.Listing{
-			Slug: slug,
-			Metadata: &pb.Listing_Metadata{
-				Version:          1,
-				AcceptedCurrency: "btc",
-				PricingCurrency:  "btc",
-				Expiry:           &timestamp.Timestamp{Seconds: 2147483647},
-				Format:           pb.Listing_Metadata_FIXED_PRICE,
-				ContractType:     pb.Listing_Metadata_ContractType(uint32(rand.Intn(3))),
-			},
-			Item: &pb.Listing_Item{
-				Sku:            sku,
-				Title:          title,
-				Tags:           tags,
-				Options:        options,
-				Nsfw:           isNSFW(),
-				Description:    fake.Paragraphs(),
-				Price:          uint64(rand.Intn(9999)),
-				ProcessingTime: strconv.Itoa(rand.Intn(14)+1) + " days",
-				Categories:     []string{categories[rand.Intn(len(categories))]},
-				Grams:          float32(rand.Intn(5000)),
-				Condition:      conditions[rand.Intn(len(conditions))],
-				Images: []*pb.Listing_Item_Image{
-					{
-						Filename: "example.jpg",
-						Original: "QmdfiTnhj1oqiCDmhxu1gdgW6ZqtR7D6ZE7j7CqWUHgKJ8",
-						Tiny:     "QmbKPEBbzVwax8rnrdxLepfmNkdTqnw2RSfJE19iax3fLK",
-						Small:    "QmQ77aAsYjs1rxcp7xZ5qUki1RGDGjQ99cok3ynUMF8Sc5",
-						Medium:   "QmVoh493xbSaKYV9yLtEapaGQ7J31gdCiDQHGSd86PNo8B",
-						Large:    "QmUWuTZhjUuY8VYWVZrcodsrsbQvckSZwTRxmJ3Avhnw1y",
-					},
-				},
-			},
-			ShippingOptions: []*pb.Listing_ShippingOption{
-				{
-					Name:    "usps",
-					Type:    pb.Listing_ShippingOption_FIXED_PRICE,
-					Regions: countries,
-					Services: []*pb.Listing_ShippingOption_Service{
-						{
-							Name:              "standard",
-							Price:             uint64(rand.Intn(999)),
-							EstimatedDelivery: fake.Digits() + " days",
-						},
-					},
-					ShippingRules: &pb.Listing_ShippingOption_ShippingRules{
-						RuleType: pb.Listing_ShippingOption_ShippingRules_FLAT_FEE_QUANTITY_RANGE,
-						Rules: []*pb.Listing_ShippingOption_ShippingRules_Rule{
-							{
-								MinRange: 0,
-								MaxRange: 99999999,
-								Price:    uint64(rand.Intn(999)),
-							},
-						},
-					},
-				},
-			},
+	imageCount := rand.Intn(6) + 1
+	images := make([]*pb.Listing_Item_Image, 0, imageCount)
+	stopGettingImages := make(chan (struct{}))
+	time.AfterFunc(10*time.Second, func() {
+		close(stopGettingImages)
+	})
 
-			TermsAndConditions: fake.Sentences(),
-			RefundPolicy:       fake.Sentence(),
+IMAGE_LOOP:
+	for i := 0; i < imageCount; i++ {
+		select {
+		case <-stopGettingImages:
+			break IMAGE_LOOP
+		case image := <-randomImages:
+			images = append(images, &pb.Listing_Item_Image{
+				Filename: image.filename,
+				Tiny:     image.Tiny,
+				Small:    image.Small,
+				Medium:   image.Medium,
+				Large:    image.Large,
+				Original: image.Original,
+			})
+		}
+	}
+
+	return &pb.Listing{
+		Slug: slug,
+		Metadata: &pb.Listing_Metadata{
+			Version:          1,
+			AcceptedCurrency: "btc",
+			PricingCurrency:  "btc",
+			Expiry:           &timestamp.Timestamp{Seconds: 2147483647},
+			Format:           pb.Listing_Metadata_FIXED_PRICE,
+			ContractType:     pb.Listing_Metadata_ContractType(uint32(rand.Intn(3))),
 		},
+		Item: &pb.Listing_Item{
+			// Skus:           sku,
+			Title:          title,
+			Tags:           tags,
+			Options:        options,
+			Nsfw:           isNSFW(),
+			Description:    fake.Paragraphs(),
+			Price:          uint64(rand.Intn(99)) + 50,
+			ProcessingTime: strconv.Itoa(rand.Intn(14)+1) + " days",
+			Categories:     []string{categories[rand.Intn(len(categories))]},
+			Grams:          float32(rand.Intn(5000)),
+			Condition:      conditions[rand.Intn(len(conditions))],
+			Images:         images,
+		},
+		ShippingOptions: []*pb.Listing_ShippingOption{
+			{
+				Name:    "usps",
+				Type:    pb.Listing_ShippingOption_FIXED_PRICE,
+				Regions: countries,
+				Services: []*pb.Listing_ShippingOption_Service{
+					{
+						Name:              "standard",
+						Price:             uint64(rand.Intn(999)),
+						EstimatedDelivery: fake.Digits() + " days",
+					},
+				},
+				ShippingRules: &pb.Listing_ShippingOption_ShippingRules{
+					RuleType: pb.Listing_ShippingOption_ShippingRules_FLAT_FEE_QUANTITY_RANGE,
+					Rules: []*pb.Listing_ShippingOption_ShippingRules_Rule{
+						{
+							MinRange: 0,
+							MaxRange: 999,
+							Price:    uint64(rand.Intn(999)),
+						},
+					},
+				},
+			},
+		},
+
+		TermsAndConditions: fake.Sentences(),
+		RefundPolicy:       fake.Sentence(),
 	}
 }
