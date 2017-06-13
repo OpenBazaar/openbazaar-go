bc206f47aec9a0112c9c25ab762ae8d0df02ea20
Chris Pacia <chris@ob1.io>
2017-06-13 12:28:55 -0400

    Do not include refundAddressTransactions object if null

--- core/utils.go
+++ core/utils.go
@@ -52,8 +52,8 @@ func ExtractIDFromPointer(pi ps.PeerInfo) (string, error) {
 }
 
 // Used by the GET order API to build transaction records suitable to be included in the order response
-func (n *OpenBazaarNode) BuildTransactionRecords(contract *pb.RicardianContract, records []*spvwallet.TransactionRecord, state pb.OrderState) (paymentRecords []*pb.TransactionRecord, refundRecord *pb.TransactionRecord, err error) {
-	refundRecord = new(pb.TransactionRecord)
+func (n *OpenBazaarNode) BuildTransactionRecords(contract *pb.RicardianContract, records []*spvwallet.TransactionRecord, state pb.OrderState) ([]*pb.TransactionRecord, *pb.TransactionRecord, error) {
+	paymentRecords := []*pb.TransactionRecord{}
 	payments := make(map[string]*pb.TransactionRecord)
 
 	// Consolidate any transactions with multiple outputs into a single record
@@ -68,16 +68,16 @@ func (n *OpenBazaarNode) BuildTransactionRecords(contract *pb.RicardianContract,
 			tx.Value = r.Value
 			ts, err := ptypes.TimestampProto(r.Timestamp)
 			if err != nil {
-				return paymentRecords, refundRecord, err
+				return paymentRecords, nil, err
 			}
 			tx.Timestamp = ts
 			ch, err := chainhash.NewHashFromStr(tx.Txid)
 			if err != nil {
-				return paymentRecords, refundRecord, err
+				return paymentRecords, nil, err
 			}
 			confirmations, height, err := n.Wallet.GetConfirmations(*ch)
 			if err != nil {
-				return paymentRecords, refundRecord, err
+				return paymentRecords, nil, err
 			}
 			tx.Height = height
 			tx.Confirmations = confirmations
@@ -87,12 +87,13 @@ func (n *OpenBazaarNode) BuildTransactionRecords(contract *pb.RicardianContract,
 	for _, rec := range payments {
 		paymentRecords = append(paymentRecords, rec)
 	}
-
+	var refundRecord *pb.TransactionRecord
 	if contract != nil && (state == pb.OrderState_REFUNDED || state == pb.OrderState_DECLINED || state == pb.OrderState_CANCELED) && contract.BuyerOrder != nil && contract.BuyerOrder.Payment != nil {
 		// For multisig we can use the outgoing from the payment address
 		if contract.BuyerOrder.Payment.Method == pb.Order_Payment_MODERATED || state == pb.OrderState_DECLINED || state == pb.OrderState_CANCELED {
 			for _, rec := range payments {
 				if rec.Value < 0 {
+					refundRecord = new(pb.TransactionRecord)
 					refundRecord.Txid = rec.Txid
 					refundRecord.Value = -rec.Value
 					refundRecord.Confirmations = rec.Confirmations
@@ -102,6 +103,7 @@ func (n *OpenBazaarNode) BuildTransactionRecords(contract *pb.RicardianContract,
 				}
 			}
 		} else if contract.Refund != nil && contract.Refund.RefundTransaction != nil && contract.Refund.Timestamp != nil {
+			refundRecord = new(pb.TransactionRecord)
 			// Direct we need to use the transaction info in the contract's refund object
 			ch, err := chainhash.NewHashFromStr(contract.Refund.RefundTransaction.Txid)
 			if err != nil {
