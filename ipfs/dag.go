1006b906c0a84c03808dd9b16d036d932196206c
Chris Pacia <chris@ob1.io>
2017-09-20 11:29:13 -0400

    Add FetchGraph function
    This is a function to walk an IPFS directory and return the CID of each file
    or sub-directory contained inside.

---
+++ ipfs/dag.go
@@ -0,0 +1,40 @@
+package ipfs
+
+import (
+	"context"
+	"github.com/ipfs/go-ipfs/merkledag"
+	"gx/ipfs/QmYhQaCYEcaPPjxJX7YcPcVKkQfRy6sJ7B3XmGFk82XYdQ/go-cid"
+	"sync"
+)
+
+// This function takes a Cid directory object and walks it returning each linked cid in the graph
+func FetchGraph(dag merkledag.DAGService, id *cid.Cid) ([]cid.Cid, error) {
+	var ret []cid.Cid
+	l := new(sync.Mutex)
+	m := make(map[string]bool)
+	ctx := context.Background()
+	m[id.String()] = true
+	for {
+		if len(m) == 0 {
+			break
+		}
+		for k := range m {
+			c, err := cid.Decode(k)
+			if err != nil {
+				return ret, err
+			}
+			ret = append(ret, *c)
+			links, err := dag.GetLinks(ctx, c)
+			if err != nil {
+				return ret, err
+			}
+			l.Lock()
+			delete(m, k)
+			for _, link := range links {
+				m[link.Cid.String()] = true
+			}
+			l.Unlock()
+		}
+	}
+	return ret, nil
+}
