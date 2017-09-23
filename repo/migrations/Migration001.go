9f147c45d41e9fe2a3890f2b1d3a607154276ea9
Chris Pacia <chris@ob1.io>
2017-09-23 11:09:11 -0400

    Add Migration001

---
+++ repo/migrations/Migration001.go
@@ -0,0 +1,166 @@
+package migrations
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path"
+)
+
+var Migration001 migration001
+
+type migration001 struct{}
+
+func (migration001) Up(repoPath string) error {
+	configFile, err := ioutil.ReadFile(path.Join(repoPath, "config"))
+	if err != nil {
+		return err
+	}
+	var cfgIface interface{}
+	json.Unmarshal(configFile, &cfgIface)
+	cfg, ok := cfgIface.(map[string]interface{})
+	if !ok {
+		return errors.New("Invalid config file")
+	}
+
+	cfg["Datastore"] = map[string]interface{}{
+		"StorageMax":         "10GB",
+		"StorageGCWatermark": 90,
+		"GCPeriod":           "1h",
+		"BloomFilterSize":    0,
+		"HashOnRead":         false,
+		"Spec": map[string]interface{}{
+			"type": "mount",
+			"mounts": []interface{}{
+				map[string]interface{}{
+					"mountpoint": "/blocks",
+					"type":       "measure",
+					"prefix":     "flatfs.datastore",
+					"child": map[string]interface{}{
+						"type":      "flatfs",
+						"path":      "blocks",
+						"sync":      true,
+						"shardFunc": "/repo/flatfs/shard/v1/next-to-last/2",
+					},
+				},
+				map[string]interface{}{
+					"mountpoint": "/",
+					"type":       "measure",
+					"prefix":     "leveldb.datastore",
+					"child": map[string]interface{}{
+						"type":        "levelds",
+						"path":        "datastore",
+						"compression": "none",
+					},
+				},
+			},
+		},
+	}
+
+	out, err := json.MarshalIndent(cfg, "", "   ")
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(path.Join(repoPath, "config"))
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(out)
+	if err != nil {
+		return err
+	}
+	f.Close()
+
+	f1, err := os.Create(path.Join(repoPath, "repover"))
+	if err != nil {
+		return err
+	}
+	_, err = f1.Write([]byte("2"))
+	if err != nil {
+		return err
+	}
+	f1.Close()
+
+	f2, err := os.Create(path.Join(repoPath, "datastore_spec"))
+	if err != nil {
+		return err
+	}
+	_, err = f2.Write([]byte(`{"mounts":[{"mountpoint":"/blocks","path":"blocks","shardFunc":"/repo/flatfs/shard/v1/next-to-last/2","type":"flatfs"},{"mountpoint":"/","path":"datastore","type":"levelds"}],"type":"mount"}`))
+	if err != nil {
+		return err
+	}
+	f2.Close()
+
+	f3, err := os.Create(path.Join(repoPath, "version"))
+	if err != nil {
+		return err
+	}
+	_, err = f3.Write([]byte(`6`))
+	if err != nil {
+		return err
+	}
+	f3.Close()
+	return nil
+}
+
+func (migration001) Down(repoPath string) error {
+	configFile, err := ioutil.ReadFile(path.Join(repoPath, "config"))
+	if err != nil {
+		return err
+	}
+	var cfgIface interface{}
+	json.Unmarshal(configFile, &cfgIface)
+	cfg, ok := cfgIface.(map[string]interface{})
+	if !ok {
+		return errors.New("Invalid config file")
+	}
+
+	cfg["Datastore"] = map[string]interface{}{
+		"BloomFilterSize": 0,
+		"GCPeriod": "1h",
+		"HashOnRead": false,
+		"NoSync": false,
+		"Params": nil,
+		"Path": path.Join(repoPath, "datastore"),
+		"StorageGCWatermark": 90,
+		"StorageMax": "10GB",
+		"Type": "leveldb",
+	}
+
+	out, err := json.MarshalIndent(cfg, "", "   ")
+	if err != nil {
+		return err
+	}
+	f, err := os.Create(path.Join(repoPath, "config"))
+	if err != nil {
+		return err
+	}
+	_, err = f.Write(out)
+	if err != nil {
+		return err
+	}
+	f.Close()
+
+	f1, err := os.Create(path.Join(repoPath, "repover"))
+	if err != nil {
+		return err
+	}
+	_, err = f1.Write([]byte("1"))
+	if err != nil {
+		return err
+	}
+	f1.Close()
+
+	f2, err := os.Create(path.Join(repoPath, "version"))
+	if err != nil {
+		return err
+	}
+	_, err = f2.Write([]byte(`5`))
+	if err != nil {
+		return err
+	}
+	f2.Close()
+	os.RemoveAll(path.Join(repoPath, "datastore_spec"))
+	return nil
+}
---
+++ repo/migrations/Migration001_test.go
@@ -0,0 +1,149 @@
+package migrations
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"strings"
+)
+
+var testConfig1 string = `{
+    "Datastore": {
+	    "BloomFilterSize": 0,
+	    "GCPeriod": "1h",
+	    "HashOnRead": false,
+	    "NoSync": false,
+	    "Params": null,
+	    "Path": "/home/chris/.openbazaar3/datastore",
+	    "StorageGCWatermark": 90,
+	    "StorageMax": "10GB",
+	    "Type": "leveldb"
+    }
+}`
+
+func TestMigration001(t *testing.T) {
+	f, err := os.Create("./config")
+	if err != nil {
+		t.Error(err)
+	}
+	f.Write([]byte(testConfig1))
+	f.Close()
+	var m migration001
+
+	// Up
+	err = m.Up("./")
+	if err != nil {
+		t.Error(err)
+	}
+	newConfig, err := ioutil.ReadFile("./config")
+	if err != nil {
+		t.Error(err)
+	}
+	newConfigCheck := `
+	   "Datastore": {
+	      "BloomFilterSize": 0,
+	      "GCPeriod": "1h",
+	      "HashOnRead": false,
+	      "Spec": {
+		 "mounts": [
+		    {
+		       "child": {
+			  "path": "blocks",
+			  "shardFunc": "/repo/flatfs/shard/v1/next-to-last/2",
+			  "sync": true,
+			  "type": "flatfs"
+		       },
+		       "mountpoint": "/blocks",
+		       "prefix": "flatfs.datastore",
+		       "type": "measure"
+		    },
+		    {
+		       "child": {
+			  "compression": "none",
+			  "path": "datastore",
+			  "type": "levelds"
+		       },
+		       "mountpoint": "/",
+		       "prefix": "leveldb.datastore",
+		       "type": "measure"
+		    }
+		 ],
+		 "type": "mount"
+	      },
+	      "StorageGCWatermark": 90,
+	      "StorageMax": "10GB"
+	   }`
+	if strings.Contains(string(newConfig), newConfigCheck) {
+		t.Error("Failed to write new Datastore")
+	}
+	repoVer, err := ioutil.ReadFile("./repover")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(repoVer) != "2" {
+		t.Error("Failed to write new repo version")
+	}
+
+	repoVer, err = ioutil.ReadFile("./version")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(repoVer) != "6" {
+		t.Error("Failed to write new ipfs repo version")
+	}
+
+	spec, err := ioutil.ReadFile("./datastore_spec")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(spec) != `{"mounts":[{"mountpoint":"/blocks","path":"blocks","shardFunc":"/repo/flatfs/shard/v1/next-to-last/2","type":"flatfs"},{"mountpoint":"/","path":"datastore","type":"levelds"}],"type":"mount"}` {
+		t.Error("Failed to write datastore_spec")
+	}
+
+	// Down
+	err = m.Down("./")
+	if err != nil {
+		t.Error(err)
+	}
+	newConfig, err = ioutil.ReadFile("./config")
+	if err != nil {
+		t.Error(err)
+	}
+	newConfigCheck = `{
+   "Datastore": {
+      "BloomFilterSize": 0,
+      "GCPeriod": "1h",
+      "HashOnRead": false,
+      "NoSync": false,
+      "Params": null,
+      "Path": "datastore",
+      "StorageGCWatermark": 90,
+      "StorageMax": "10GB",
+      "Type": "leveldb"
+   }
+}`
+	if !strings.Contains(string(newConfig), newConfigCheck) {
+		t.Error("Failed to write new Datastore")
+	}
+	repoVer, err = ioutil.ReadFile("./repover")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(repoVer) != "1" {
+		t.Error("Failed to write new repo version")
+	}
+
+	repoVer, err = ioutil.ReadFile("./version")
+	if err != nil {
+		t.Error(err)
+	}
+	if string(repoVer) != "5" {
+		t.Error("Failed to write new ipfs repo version")
+	}
+
+	os.Remove("./config")
+	os.Remove("./repover")
+	os.Remove("./version")
+	os.Remove("./datastore_spec")
+}
+
