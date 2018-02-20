9ae37e4902d512d27295e259d97d418c00a87624
Chris Pacia <chris@ob1.io>
2018-02-20 11:01:47 -0500

    Check for regtest params when running validateListing function

--- core/order.go
+++ core/order.go
@@ -513,7 +513,7 @@ func (n *OpenBazaarNode) createContractWithOrder(data *PurchaseData) (*pb.Ricard
 			if err := validateVendorID(sl.Listing); err != nil {
 				return nil, err
 			}
-			if err := validateListing(sl.Listing, n.TestNetworkEnabled()); err != nil {
+			if err := validateListing(sl.Listing, n.TestNetworkEnabled() || n.RegressionNetworkEnabled()); err != nil {
 				return nil, fmt.Errorf("Listing failed to validate, reason: %q", err.Error())
 			}
 			if err := verifySignaturesOnListing(sl); err != nil {
