e133983c94f5c9405aed4e716f18525e504647a2
Mike Greenberg <mikeg@ob1.io>
2018-05-03 15:55:04 -0400

    Refactor repo.Notification test examples

--- repo/notifications_test.go
+++ repo/notifications_test.go
@@ -9,109 +9,13 @@ import (
 	"github.com/OpenBazaar/openbazaar-go/repo"
 )
 
+// TestNotificationMarshalling ensures that the new Notification marshal format is
+// functioning properly. This applies to notifications which have been marshalled in
+// the datastore with json.Marshal(Notification{}). Some notifications have been
+// marshalled in the datastore with json.Marshal(Notification{}.NotifierData), and
+// TestLegacyNotificationMarshalling covers those cases.
 func TestNotificationMarshalling(t *testing.T) {
-	var exampleNotifications = []repo.Notifier{
-		repo.CompletionNotification{
-			ID:      "orderCompletionID",
-			Type:    repo.NotifierTypeCompletionNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.DisputeAcceptedNotification{
-			ID:      "disputeAcceptedID",
-			Type:    repo.NotifierTypeDisputeAcceptedNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.DisputeCloseNotification{
-			ID:      "disputeCloseID",
-			Type:    repo.NotifierTypeDisputeCloseNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.DisputeOpenNotification{
-			ID:      "disputeOpenID",
-			Type:    repo.NotifierTypeDisputeOpenNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.DisputeUpdateNotification{
-			ID:      "disputeUpdateID",
-			Type:    repo.NotifierTypeDisputeUpdateNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.FollowNotification{
-			ID:     "followID",
-			Type:   repo.NotifierTypeFollowNotification,
-			PeerId: repo.NewNotificationID(),
-		},
-		repo.FulfillmentNotification{
-			ID:      "fulfillmentID",
-			Type:    repo.NotifierTypeFulfillmentNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.ModeratorAddNotification{
-			ID:     "moderatorAddID",
-			Type:   repo.NotifierTypeModeratorAddNotification,
-			PeerId: repo.NewNotificationID(),
-		},
-		repo.ModeratorDisputeExpiry{
-			ID:     "disputeNotificationID",
-			Type:   repo.NotifierTypeModeratorDisputeExpiry,
-			CaseID: repo.NewNotificationID(),
-		},
-		repo.ModeratorRemoveNotification{
-			ID:     "moderatorRemoveID",
-			Type:   repo.NotifierTypeModeratorRemoveNotification,
-			PeerId: repo.NewNotificationID(),
-		},
-		repo.OrderCancelNotification{
-			ID:      "orderCancelID",
-			Type:    repo.NotifierTypeOrderCancelNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.OrderConfirmationNotification{
-			ID:      "orderConfirmID",
-			Type:    repo.NotifierTypeOrderConfirmationNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.OrderDeclinedNotification{
-			ID:      "orderDeclinedID",
-			Type:    repo.NotifierTypeOrderDeclinedNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.OrderNotification{
-			ID:      "orderNotificationID",
-			Type:    repo.NotifierTypeOrderNewNotification,
-			BuyerID: repo.NewNotificationID(),
-		},
-		repo.PaymentNotification{
-			ID:      "paymentID",
-			Type:    repo.NotifierTypePaymentNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.ProcessingErrorNotification{
-			ID:      "processingErrorID",
-			Type:    repo.NotifierTypeProcessingErrorNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.BuyerDisputeTimeout{
-			ID:      "buyerDisputeID",
-			Type:    repo.NotifierTypeBuyerDisputeTimeout,
-			OrderID: repo.NewNotificationID(),
-		},
-		repo.RefundNotification{
-			ID:      "refundID",
-			Type:    repo.NotifierTypeRefundNotification,
-			OrderId: repo.NewNotificationID(),
-		},
-		repo.VendorDisputeTimeout{
-			ID:      "saleAgingID",
-			Type:    repo.NotifierTypeVendorDisputeTimeout,
-			OrderID: repo.NewNotificationID(),
-		},
-		repo.UnfollowNotification{
-			ID:     "unfollowID",
-			Type:   repo.NotifierTypeUnfollowNotification,
-			PeerId: repo.NewNotificationID(),
-		},
-	}
+	var exampleNotifications = createNotificationExmaples()
 
 	for _, n := range exampleNotifications {
 		var (
@@ -141,8 +45,66 @@ func TestNotificationMarshalling(t *testing.T) {
 	}
 }
 
+// TestLegacyNotificationMarshalling ensures that the legacy Notification marshalling is
+// functioning properly. This applies to notifications which have been marshalled in
+// the datastore with json.Marshal(Notification{}.NotifierData).
 func TestLegacyNotificationMarshalling(t *testing.T) {
-	var exampleNotifications = []repo.Notifier{
+	var exampleNotifications = createLegacyNotificationExamples()
+	for _, n := range exampleNotifications {
+		var (
+			actual = &repo.Notification{}
+		)
+		data, err := json.Marshal(n)
+		if err != nil {
+			t.Errorf("failed marshaling '%s': %s\n", n.GetType(), err)
+			continue
+		}
+		if err := json.Unmarshal(data, actual); err != nil {
+			t.Errorf("failed unmarshaling '%s': %s\n", n.GetType(), err)
+			continue
+		}
+
+		if actual.GetID() != n.GetID() {
+			t.Error("Expected notification to match ID, but did not")
+			t.Errorf("Expected: %s\n", n.GetID())
+			t.Errorf("Actual: %s\n", actual.GetID())
+		}
+		if actual.GetType() != n.GetType() {
+			t.Error("Expected notification to match types, but did not")
+			t.Errorf("Expected: %s\n", n.GetType())
+			t.Errorf("Actual: %s\n", actual.GetType())
+		}
+		if reflect.DeepEqual(actual.NotifierData, n) != true {
+			t.Error("Expected notifier data to match, but did not")
+			t.Errorf("Expected: %+v\n", n)
+			t.Errorf("Actual: %+v\n", actual.NotifierData)
+		}
+	}
+}
+
+func createNotificationExmaples() []repo.Notifier {
+	return append([]repo.Notifier{
+		repo.ModeratorDisputeExpiry{
+			ID:     "disputeNotificationID",
+			Type:   repo.NotifierTypeModeratorDisputeExpiry,
+			CaseID: repo.NewNotificationID(),
+		},
+		repo.BuyerDisputeTimeout{
+			ID:      "buyerDisputeID",
+			Type:    repo.NotifierTypeBuyerDisputeTimeout,
+			OrderID: repo.NewNotificationID(),
+		},
+		repo.VendorDisputeTimeout{
+			ID:      "saleAgingID",
+			Type:    repo.NotifierTypeVendorDisputeTimeout,
+			OrderID: repo.NewNotificationID(),
+		},
+	},
+		createLegacyNotificationExamples()...)
+}
+
+func createLegacyNotificationExamples() []repo.Notifier {
+	return []repo.Notifier{
 		repo.CompletionNotification{
 			ID:      "orderCompletionID",
 			Type:    repo.NotifierTypeCompletionNotification,
@@ -228,37 +190,6 @@ func TestLegacyNotificationMarshalling(t *testing.T) {
 			Type:   repo.NotifierTypeUnfollowNotification,
 			PeerId: repo.NewNotificationID(),
 		},
-	}
-
-	for _, n := range exampleNotifications {
-		var (
-			actual = &repo.Notification{}
-		)
-		data, err := json.Marshal(n)
-		if err != nil {
-			t.Errorf("failed marshaling '%s': %s\n", n.GetType(), err)
-			continue
-		}
-		if err := json.Unmarshal(data, actual); err != nil {
-			t.Errorf("failed unmarshaling '%s': %s\n", n.GetType(), err)
-			continue
-		}
-
-		if actual.GetID() != n.GetID() {
-			t.Error("Expected notification to match ID, but did not")
-			t.Errorf("Expected: %s\n", n.GetID())
-			t.Errorf("Actual: %s\n", actual.GetID())
-		}
-		if actual.GetType() != n.GetType() {
-			t.Error("Expected notification to match types, but did not")
-			t.Errorf("Expected: %s\n", n.GetType())
-			t.Errorf("Actual: %s\n", actual.GetType())
-		}
-		if reflect.DeepEqual(actual.NotifierData, n) != true {
-			t.Error("Expected notifier data to match, but did not")
-			t.Errorf("Expected: %+v\n", n)
-			t.Errorf("Actual: %+v\n", actual.NotifierData)
-		}
 	}
 }
 
