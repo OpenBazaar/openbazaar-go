10e1ee71adab2a5e07f159bfd1eb0941e6f5afa3
Vadim <chuvak117@gmail.com>
2018-05-06 16:45:26 +0300

    v.2

--- cmd/gencerts.go
+++ cmd/gencerts.go
@@ -1,11 +1,32 @@
 package cmd
 
 import (
+	"github.com/OpenBazaar/openbazaar-go/repo"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
 	"os/exec"
 	"runtime"
 )
 
-func Execute(args []string) error {
+type GenerateCertificates struct {
+	DataDir string `short:"d" long:"datadir" description:"specify the data directory to be used"`
+	Testnet bool   `short:"t" long:"testnet" description:"config file is for testnet node"`
+}
+
+func (x *GenerateCertificates) Execute(args []string) error {
+
+	// Set repo path
+	repoPath, err := repo.GetRepoPath(x.Testnet)
+	if err != nil {
+		return err
+	}
+	if x.DataDir != "" {
+		repoPath = x.DataDir
+	}
+	r, err := fsrepo.Open(repoPath)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
 
 	var command []string
 	switch runtime.GOOS {
@@ -17,7 +38,9 @@ func Execute(args []string) error {
 		command = []string{`sh`, `-c`}
 	}
 	cmd := exec.Command(command[0], append(command[1:], `openssl req -newkey rsa:2048 -sha256 -nodes -keyout key.pem -x509 -days 365 -out cert.pem -subj "/C=. /ST=. /L=. /O=. Company/CN=."`)...)
-	cmd.Start()
+	cmd.Run()
+
+	log.Notice("Done")
 
 	return nil
 
