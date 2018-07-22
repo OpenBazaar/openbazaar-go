7d437adb056f19fa910f87d5642c94087555d0b9
Chris Pacia <chris@ob1.io>
2018-07-22 12:49:21 -0400

    Add migration 10 to migration list and initialize

--- repo/init.go
+++ repo/init.go
@@ -17,7 +17,7 @@ import (
 	"time"
 )
 
-const RepoVersion = "10"
+const RepoVersion = "11"
 
 var log = logging.MustGetLogger("repo")
 var ErrRepoExists = errors.New("IPFS configuration file exists. Reinitializing would overwrite your keys. Use -f to force overwrite.")
