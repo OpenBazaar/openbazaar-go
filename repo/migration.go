3326736d6b1db74751920cd98a2b7ebfcdef57fb
Chris Pacia <chris@ob1.io>
2018-07-23 08:47:46 -0400

    Format repover string in migration

--- repo/migration.go
+++ repo/migration.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 )
 
 type Migration interface {
@@ -35,6 +36,9 @@ func MigrateUp(repoPath, dbPassword string, testnet bool) error {
 		return err
 	} else if err != nil && os.IsNotExist(err) {
 		version = []byte("0")
+	}
+	if strings.Contains(string(version), "\n") {
+		version = []byte(strings.Replace(string(version), "\n", "", -1))
 	}
 	v, err := strconv.Atoi(string(version))
 	if err != nil {
