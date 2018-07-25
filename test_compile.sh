8307b4a355593360f940770aa5fa427ec17b2238
Tyler Smith <tylersmith.me@gmail.com>
2018-07-24 20:44:59 -0700

    BUILD,REFACTOR: Simplify test_compile.sh.

--- test_compile.sh
+++ test_compile.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
-
 set -e
-pwd
 go test -i
-go test -coverprofile=api.cover.out ./api
-go test -coverprofile=bitcoin.cover.out ./bitcoin
-go test -coverprofile=bitcoin.listeners.cover.out ./bitcoin/listeners
-go test -coverprofile=bitcoin.bitcoind.cover.out ./bitcoin
-go test -coverprofile=core.cover.out ./core
-go test -coverprofile=ipfs.cover.out ./ipfs
-go test -coverprofile=net.cover.out ./net
-go test -coverprofile=net.service.cover.out ./net/service
-go test -coverprofile=net.repointer.cover.out ./net/repointer
-go test -coverprofile=net.retriever.cover.out ./net/retriever
-go test -coverprofile=repo.cover.out ./repo
-go test -coverprofile=repo.db.cover.out ./repo/db
-go test -coverprofile=repo.migrations.db.cover.out ./repo/migrations
-go test -coverprofile=schema.cover.out ./schema
-go test -coverprofile=storage.cover.out ./storage
-go test -coverprofile=storage.dropbox.cover.out ./storage/dropbox
-go test -coverprofile=storage.selfhosted.cover.out ./storage/selfhosted
-echo "mode: set" > coverage.out && cat *.cover.out | grep -v mode: | sort -r | \
-awk '{if($1 != last) {print $0;last=$1}}' >> coverage.out
-rm -rf *.cover.out
+go test -coverprofile=coverage.out ./...
 
 
 # Maybe activate someday, but right now this takes a long time and has a hard time
