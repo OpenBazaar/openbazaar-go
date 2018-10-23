baf4113467b7d7e197db0110f97e206013cf216a
Chris Pacia <chris@ob1.io>
2018-10-23 16:33:56 -0400

    Update listings.py test

--- qa/listings.py
+++ qa/listings.py
@@ -35,9 +35,8 @@ class ListingsTest(OpenBazaarTestFramework):
         # POST listing
         with open('testdata/listing.json') as listing_file:
             ljson = json.load(listing_file, object_pairs_hook=OrderedDict)
-        if self.bitcoincash:
-            ljson["metadata"]["pricingCurrency"] = "tbch"
-            currency = "tbch"
+        ljson["metadata"]["pricingCurrency"] = "T" + self.cointype
+        currency = "T" + self.cointype
         api_url = vendor["gateway_url"] + "ob/listing"
         r = requests.post(api_url, data=json.dumps(ljson, indent=4))
         if r.status_code == 200:
@@ -62,8 +61,8 @@ class ListingsTest(OpenBazaarTestFramework):
             raise TestFailure("ListingsTest - FAIL: One listing should be returned")
 
         listing = resp[0]
-        if listing["acceptedCurrencies"] != [currency]:
-            raise TestFailure("ListingsTest - FAIL: Listing should have acceptedCurrences")
+        if currency.lower() not in listing["acceptedCurrencies"]:
+            raise TestFailure("ListingsTest - FAIL: Listing should have acceptedCurrencies")
 
         # listing show endpoint returning correct data
         slug = listing["slug"]
@@ -76,7 +75,7 @@ class ListingsTest(OpenBazaarTestFramework):
             raise TestFailure("ListingsTest - FAIL: Listings GET failed. Reason: %s", resp["reason"])
 
         resp = json.loads(r.text)
-        if resp["listing"]["metadata"]["acceptedCurrencies"] != [currency.upper()]:
+        if currency.lower() not in resp["listing"]["metadata"]["acceptedCurrencies"]:
             raise TestFailure("ListingsTest - FAIL: Listing should have acceptedCurrences in metadata")
 
         # check vendor's index from another node
@@ -90,7 +89,7 @@ class ListingsTest(OpenBazaarTestFramework):
         resp = json.loads(r.text)
         if len(resp) != 1:
             raise TestFailure("ListingsTest - FAIL: One listing should be returned")
-        if resp[0]["acceptedCurrencies"] != [currency]:
+        if currency.lower() not in resp[0]["acceptedCurrencies"]:
             raise TestFailure("ListingsTest - FAIL: Listing should have acceptedCurrences")
 
         # check listing show page from another node
@@ -103,7 +102,7 @@ class ListingsTest(OpenBazaarTestFramework):
             raise TestFailure("ListingsTest - FAIL: Listings GET failed. Reason: %s", resp["reason"])
 
         resp = json.loads(r.text)
-        if resp["listing"]["metadata"]["acceptedCurrencies"] != [currency.upper()]:
+        if currency.lower() not in resp["listing"]["metadata"]["acceptedCurrencies"]:
             raise TestFailure("ListingsTest - FAIL: Listing should have acceptedCurrences in metadata")
 
         print("ListingsTest - PASS")
