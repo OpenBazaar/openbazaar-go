3d390c9857045673cdfb016abf7e208ed0d005bf
michizhou <33012425+michizhou@users.noreply.github.com>
2018-10-27 19:09:36 -0400

    Removed unused library reference

--- core/disputes.go
+++ core/disputes.go
@@ -8,7 +8,6 @@ import (
 	peer "gx/ipfs/QmZoWKhxUmZ2seW4BzX6fJkNR8hh9PsGModr7q171yq2SS/go-libp2p-peer"
 	libp2p "gx/ipfs/QmaPbCnUMBohSGo3KnxEa2bHqyJVVeEEcwtqJAYxerieBo/go-libp2p-crypto"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
