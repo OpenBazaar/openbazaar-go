1c4ea9b9159d909b6183fc3121c4d8b2e434c960
Brian Hoffman <brianchoffman@gmail.com>
2018-11-13 16:00:27 -0500

    Refactor unpin directory

--- ipfs/pin.go
+++ ipfs/pin.go
@@ -2,14 +2,16 @@ package ipfs
 
 import (
 	"context"
+	"github.com/ipfs/go-ipfs/core/coreapi"
 
 	"github.com/ipfs/go-ipfs/core"
-	"github.com/ipfs/go-ipfs/core/corerepo"
+	corerepo "github.com/ipfs/go-ipfs/core/corerepo"
 )
 
 /* Recursively un-pin a directory given its hash.
    This will allow it to be garbage collected. */
 func UnPinDir(n *core.IpfsNode, rootHash string) error {
-	_, err := corerepo.Unpin(n, context.Background(), []string{"/ipfs/" + rootHash}, true)
+	api := coreapi.NewCoreAPI(n)
+	_, err := corerepo.Unpin(n, api, context.Background(), []string{"/ipfs/" + rootHash}, true)
 	return err
 }
