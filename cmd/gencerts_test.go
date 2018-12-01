0f4807a83d55d7a80d491556bfdfcbed02257182
allen-munsch <enderplux@protonmail.com>
2018-12-01 12:08:31 -0600

    am-949: review changes formatting, readability, coding styles

--- cmd/gencerts_test.go
+++ cmd/gencerts_test.go
@@ -25,11 +25,12 @@ func buildCertDirectory() (*string, func(), error) {
 }
 
 func TestGenCertsGenericDefaults(t *testing.T) {
-	var dataPath, Destroy, schema_err = buildCertDirectory()
+	var dataPath, destroy, schemaErr = buildCertDirectory()
+	defer destroy()
 	dataDir := string([]byte(*dataPath))
-	if schema_err != nil {
-		Destroy()
-		t.Fatal(schema_err)
+	if schemaErr != nil {
+		destroy()
+		t.Fatal(schemaErr)
 	}
 
 	config := cmd.GenerateCertificates{
@@ -39,84 +40,70 @@ func TestGenCertsGenericDefaults(t *testing.T) {
 		ValidFor: 1, // 1ns ... 1*1e9 == 1s
 	}
 
-	args := []string{"args?"}
+	args := []string{"args"} // "Commander" interface args
 
 	if err := config.Execute(args); err != nil {
-		Destroy()
-		t.Fatal(err)
+		t.Fatalf("unable to GenerateCertificates: %s", err)
 	}
 
 	sslPath := path.Join(dataDir, "ssl")
-	if fileInfo, err := os.Stat(sslPath); err != nil {
-		Destroy()
-		t.Fatal(err)
-	} else {
-		if fileInfo.Mode().Perm() != 0755 {
-			Destroy()
-			t.Fatal("ssl directory does not have 0755 permissions")
-		}
-		if !fileInfo.IsDir() {
-			Destroy()
-			t.Fatalf("Expecting a directory: %s", dataDir)
-		}
-	}
+	fileInfoSsl, errSsl := os.Stat(sslPath)
+    if errSsl != nil {
+        t.Fatalf("unable to find sslPath: %s", errSsl)
+    }
+    if fileInfoSsl.Mode().Perm() != 0755 {
+        t.Fatal("ssl directory does not have 0755 permissions")
+    }
+    if !fileInfoSsl.IsDir() {
+        t.Fatalf("Expecting a directory: %s", dataDir)
+    }
 
 	certPemPath := path.Join(dataDir, "ssl", "cert.pem")
-	if fileInfo, err := os.Stat(certPemPath); err != nil {
-		Destroy()
-		t.Fatal(err)
-	} else {
-		if fileInfo.Mode().Perm() != 0644 {
-			Destroy()
-			t.Fatal("cert.pem does not have 0644 permissions")
-		}
-		if !fileInfo.Mode().IsRegular() {
-			Destroy()
-			t.Fatalf("Expecting a file: %s", certPemPath)
-		}
+	fileInfoCert, errCert := os.Stat(certPemPath)
+	if errCert != nil {
+		t.Fatalf("unable to find certPemPath %s: %s", certPemPath, errCert)
 	}
+	if fileInfoCert.Mode().Perm() != 0644 {
+			t.Fatal("cert.pem does not have 0644 permissions")
+    }
+    if !fileInfoCert.Mode().IsRegular() {
+        t.Fatalf("Expecting a file: %s", certPemPath)
+    }
 
 	keyPemPath := path.Join(dataDir, "ssl", "key.pem")
-	if fileInfo, err := os.Stat(keyPemPath); err != nil {
-		Destroy()
-		t.Fatal(err)
-	} else {
-		if fileInfo.Mode().Perm() != 0600 {
-			Destroy()
-			t.Fatal("cert.pem does not have 0600 permissions")
-		}
-		if !fileInfo.Mode().IsRegular() {
-			Destroy()
-			t.Fatalf("Expecting a file: %s", keyPemPath)
-		}
+	fileInfoKey, errKey := os.Stat(keyPemPath)
+	if errKey != nil {
+		t.Fatalf("unable to find keyPemPath: %s", errKey)
 	}
+    if fileInfoKey.Mode().Perm() != 0600 {
+        t.Fatal("cert.pem does not have 0600 permissions")
+    }
+    if !fileInfoKey.Mode().IsRegular() {
+        t.Fatalf("Expecting a file: %s", keyPemPath)
+    }
 
 	certPemBytes, err := ioutil.ReadFile(certPemPath)
 	if err != nil {
-		Destroy()
-		t.Fatal(err)
+		t.Fatalf("unable to read certPemPath: %s", err)
 	}
 	block, _ := pem.Decode(certPemBytes)
 	if block == nil {
-		Destroy()
 		t.Fatal("failed to parse PEM block containing the public key")
 	}
 
 	pub, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		Destroy()
 		t.Fatal("failed to parse DER encoded public key: " + err.Error())
 	}
 	if pub.Issuer.String() != "O=OpenBazaar" {
-		t.Errorf("%s != OpenBazaar", pub.Issuer.String())
+		t.Errorf("unexpected issuer found on certificate: %s != OpenBazaar", pub.Issuer.String())
 	}
 	// t.Log(pub.IssuingCertificateURL)
 	if err := pub.VerifyHostname("127.0.0.1"); err != nil {
-		t.Errorf("%s", err)
+		t.Errorf("unable to VerifyHostname 127.0.0.1: %s", err)
 	}
 
 	if pub.NotAfter.Sub(pub.NotBefore).Seconds() != 0 {
 		t.Errorf("Certificate NotAfter != NotBefore: %s %s", pub.NotAfter.String(), pub.NotBefore.String())
 	}
-	Destroy()
 }
