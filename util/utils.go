284257e322e280675a2b9aa6d83966700be9461b
Ashwin Mangale <ashwin.mangale@gmail.com>
2019-01-24 21:35:30 +0530

    lint fix

--- util/utils.go
+++ util/utils.go
@@ -4,10 +4,12 @@ import (
 	"strings"
 )
 
+// NormalizeAddress is used to strip the 0x prefix
 func NormalizeAddress(addr string) string {
 	return strings.Replace(addr, "0x", "", 1)
 }
 
+// AreAddressesEqual - check if addresses are equal after normalizing them
 func AreAddressesEqual(addr1, addr2 string) bool {
 	return NormalizeAddress(addr1) == NormalizeAddress(addr2)
 }
