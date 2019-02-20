31ef6492311800cc94e3f9c26a82a8a4bb3c7d01
Mike Greenberg <mikeg@ob1.io>
2019-02-19 20:36:08 -0500

    Add CurrencyValue to handle rate conversions

--- repo/currency.go
+++ repo/currency.go
@@ -2,6 +2,8 @@ package repo
 
 import (
 	"errors"
+	"fmt"
+	"math/big"
 	"runtime/debug"
 	"strings"
 )
@@ -14,9 +16,16 @@ const (
 var (
 	ErrCurrencyCodeLengthInvalid     = errors.New("invalid length for currency code, must be three characters or four characters and begin with a 'T'")
 	ErrCurrencyCodeTestSymbolInvalid = errors.New("invalid test indicator for currency code, four characters must begin with a 'T'")
+	ErrInsufficientPrecision         = errors.New("unable to accurately represent value as int64")
+	ErrNegativeRate                  = errors.New("convertion rate must be greater than zero")
 )
 
-type CurrencyCode string
+type (
+	CurrencyCode  string
+	CurrencyValue struct {
+		Amount *big.Int
+	}
+)
 
 func NewCurrencyCode(c string) (*CurrencyCode, error) {
 	if len(c) < CurrencyCodeValidMinimumLength || len(c) > CurrencyCodeValidMaximumLength {
@@ -36,4 +45,70 @@ func (c *CurrencyCode) String() string {
 		return "nil"
 	}
 	return string(*c)
+}
+
+func NewCurrencyValueFromInt(amount int64, currencyCode string) (*CurrencyValue, error) {
+	var i = new(big.Int)
+	i.SetInt64(amount)
+	return &CurrencyValue{Amount: i}, nil
+}
+
+func NewCurrencyValueFromUint(amount uint64, currencyCode string) (*CurrencyValue, error) {
+	var i = new(big.Int)
+	i.SetUint64(amount)
+	return &CurrencyValue{Amount: i}, nil
+}
+
+func NewCurrencyValue(amount, currencyCode string) (*CurrencyValue, error) {
+	var (
+		i  = new(big.Int)
+		ok bool
+	)
+	if i, ok = i.SetString(amount, 0); !ok {
+		return nil, fmt.Errorf("invalid amount")
+	}
+	return &CurrencyValue{Amount: i}, nil
+}
+
+func (v *CurrencyValue) AmountInt64() (int64, error) {
+	if !v.Amount.IsInt64() {
+		return 0, ErrInsufficientPrecision
+	}
+	return v.Amount.Int64(), nil
+}
+
+func (v *CurrencyValue) AmountUint64() (uint64, error) {
+	if !v.Amount.IsUint64() {
+		return 0, ErrInsufficientPrecision
+	}
+	return v.Amount.Uint64(), nil
+}
+
+func (v *CurrencyValue) String() string {
+	return v.Amount.String()
+}
+
+func (v *CurrencyValue) Equal(other *CurrencyValue) bool {
+	if v == nil && other == nil {
+		return true
+	}
+	if v == nil || other == nil {
+		return false
+	}
+	return v.Amount.Cmp(other.Amount) == 0
+}
+
+func (v *CurrencyValue) ConvertTo(currencyCode string, exchangeRate float64) (*CurrencyValue, error) {
+	if exchangeRate <= 0 {
+		return nil, ErrNegativeRate
+	}
+	var (
+		j    = new(big.Float)
+		rate = new(big.Float)
+	)
+	j.SetInt(v.Amount)
+	rate.SetFloat64(exchangeRate)
+	j.Mul(j, rate)
+	result, _ := j.Int(nil)
+	return &CurrencyValue{Amount: result}, nil
 }
--- repo/currency_test.go
+++ repo/currency_test.go
@@ -1,10 +1,21 @@
 package repo_test
 
 import (
+	"encoding/json"
+	"strings"
 	"testing"
 
 	"github.com/OpenBazaar/openbazaar-go/repo"
 )
+
+func mustNewCurrencyValue(t *testing.T, amount, currencyCode string) *repo.CurrencyValue {
+	var c, err = repo.NewCurrencyValue(amount, currencyCode)
+	if err != nil {
+		t.Fatalf("new currency value: %s", err.Error())
+		return nil
+	}
+	return c
+}
 
 func TestNewCurrencyCode(t *testing.T) {
 	var examples = []struct {
@@ -58,6 +69,177 @@ func TestNewCurrencyCode(t *testing.T) {
 		}
 		if c.String() != e.expectString {
 			t.Errorf("unexpected string for input (%s): have (%s), want (%s)", e.input, c.String(), e.expectString)
+		}
+	}
+}
+
+func TestCurrencyValueMarshalsToJSON(t *testing.T) {
+	var (
+		invalidAmountErr = "invalid amount"
+		examples         = []struct {
+			value     string
+			expectErr *string
+		}{
+			{
+				value:     "123456789012345678",
+				expectErr: nil,
+			},
+			{
+				value:     "123456789012345678901234567890",
+				expectErr: nil,
+			},
+			{
+				value:     "",
+				expectErr: &invalidAmountErr,
+			},
+		}
+	)
+
+	for _, e := range examples {
+		var (
+			example, err = repo.NewCurrencyValue(e.value, "BTC")
+			actual       *repo.CurrencyValue
+		)
+		if e.expectErr == nil && err != nil {
+			t.Errorf("unable to parse valid input '%s': %s", e.value, err.Error())
+			continue
+		} else {
+			if err != nil && !strings.Contains(err.Error(), *e.expectErr) {
+				t.Errorf("expected error to contain (%s), was: %s", *e.expectErr, err.Error())
+			}
+			continue
+		}
+		j, err := json.Marshal(example)
+		if err != nil {
+			t.Errorf("marshaling %s: %s", example.String(), err)
+			continue
+		}
+
+		if err := json.Unmarshal(j, &actual); err != nil {
+			t.Errorf("unmarhsaling %s, %s", example.String(), err)
+			continue
+		}
+
+		if !actual.Equal(example) {
+			t.Errorf("expected %s and %s to be equal, but was not", example.String(), actual.String())
+		}
+	}
+}
+
+func TestCurrencyValuesAreEqual(t *testing.T) {
+	var examples = []struct {
+		value    *repo.CurrencyValue
+		other    *repo.CurrencyValue
+		expected bool
+	}{
+		{
+			value:    nil,
+			other:    nil,
+			expected: true,
+		},
+		{
+			value:    mustNewCurrencyValue(t, "1", "BTC"),
+			other:    mustNewCurrencyValue(t, "1", "BTC"),
+			expected: true,
+		},
+		{
+			value:    nil,
+			other:    mustNewCurrencyValue(t, "1", "BTC"),
+			expected: false,
+		},
+		{
+			value:    mustNewCurrencyValue(t, "1", "BTC"),
+			other:    nil,
+			expected: false,
+		},
+		{
+			value:    mustNewCurrencyValue(t, "1", "BTC"),
+			other:    mustNewCurrencyValue(t, "2", "BTC"),
+			expected: false,
+		},
+	}
+
+	for _, c := range examples {
+		if c.value.Equal(c.other) != c.expected {
+			if c.expected {
+				t.Errorf("expected %s to equal %s but did not", c.value.String(), c.other.String())
+			} else {
+				t.Errorf("expected %s to not equal %s but did", c.value.String(), c.other.String())
+			}
+		}
+	}
+}
+
+func TestCurrencyValuesConvertCorrectly(t *testing.T) {
+	var (
+		zeroRateErr = "rate must be greater than zero"
+
+		examples = []struct {
+			value        int64
+			exchangeRate float64
+			expected     int64
+			expectedErr  *string
+		}{
+			{ // errors zero rate
+				value:        123,
+				exchangeRate: 0,
+				expected:     0,
+				expectedErr:  &zeroRateErr,
+			},
+			{ // errors negative rate
+				value:        123,
+				exchangeRate: -0.1,
+				expected:     0,
+				expectedErr:  &zeroRateErr,
+			},
+			{ // rounds down
+				value:        1,
+				exchangeRate: 0.9,
+				expected:     0,
+				expectedErr:  nil,
+			},
+			{ // handles negative values
+				value:        -100,
+				exchangeRate: 0.123,
+				expected:     -12,
+				expectedErr:  nil,
+			},
+			{ // handles zero
+				value:        0,
+				exchangeRate: 0.99999,
+				expected:     0,
+				expectedErr:  nil,
+			},
+		}
+	)
+
+	for _, e := range examples {
+		before, err := repo.NewCurrencyValueFromInt(e.value, "AAA")
+		if err != nil {
+			t.Errorf("unable to create CurrencyValue for value (%d): %s", e.value, err.Error())
+			continue
+		}
+		actual, err := before.ConvertTo("BBB", e.exchangeRate)
+		if err != nil {
+			if e.expectedErr != nil && !strings.Contains(err.Error(), *e.expectedErr) {
+				t.Errorf("expected value (%d) to error with (%s) but returned: %s", e.value, *e.expectedErr, err.Error())
+			}
+			continue
+		} else {
+			if e.expectedErr != nil {
+				t.Errorf("expected error (%s) but produced none", *e.expectedErr)
+				t.Logf("\tfor example: %+v", e)
+			}
+		}
+
+		intAmount, err := actual.AmountInt64()
+		if err != nil {
+			t.Errorf("error producing int64 amount: %s", err.Error())
+			continue
+		}
+		if intAmount != e.expected {
+			t.Errorf("expected converted value to be %d, but was %d", e.expected, intAmount)
+			continue
 		}
 	}
 }
