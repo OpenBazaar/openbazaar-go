22118dbcb2e775fd70132f8afab293579cde80ed
Mike Greenberg <mikeg@ob1.io>
2019-03-04 13:17:15 -0500

    Fix lint report error; Simplify Query function

--- ipfs/query.go
+++ ipfs/query.go
@@ -2,28 +2,25 @@ package ipfs
 
 import (
 	"context"
+
 	routing "gx/ipfs/QmPpYHPRGVpSJTkQDQDwTYZ1cYUR2NM4HS6M3iAXi8aoUa/go-libp2p-kad-dht"
-	"gx/ipfs/QmTRhk7cgjUf2gfQ3p2M9KPECNZEW9XUrmHcFCgog4cPgB/go-libp2p-peer"
+	peer "gx/ipfs/QmTRhk7cgjUf2gfQ3p2M9KPECNZEW9XUrmHcFCgog4cPgB/go-libp2p-peer"
 )
 
+// Query returns the closest peers known for peerID
 func Query(dht *routing.IpfsDHT, peerID string) ([]peer.ID, error) {
 	id, err := peer.IDB58Decode(peerID)
 	if err != nil {
 		return nil, err
 	}
-
 	ch, err := dht.GetClosestPeers(context.Background(), string(id))
 	if err != nil {
 		return nil, err
 	}
+
 	var closestPeers []peer.ID
-	events := make(chan struct{})
-	go func() {
-		defer close(events)
-		for p := range ch {
-			closestPeers = append(closestPeers, p)
-		}
-	}()
-	<-events
+	for p := range ch {
+		closestPeers = append(closestPeers, p)
+	}
 	return closestPeers, nil
 }
