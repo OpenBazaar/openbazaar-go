977b3567fef3d581ea57492ca5a989124b3fd7c0
Chris Pacia <chris@ob1.io>
2019-03-04 08:54:40 -0500

    Fix bug performing type check on DHT
    closes #1478

--- ipfs/query.go
+++ ipfs/query.go
@@ -2,19 +2,11 @@ package ipfs
 
 import (
 	"context"
-	"errors"
-
 	routing "gx/ipfs/QmPpYHPRGVpSJTkQDQDwTYZ1cYUR2NM4HS6M3iAXi8aoUa/go-libp2p-kad-dht"
-	peer "gx/ipfs/QmTRhk7cgjUf2gfQ3p2M9KPECNZEW9XUrmHcFCgog4cPgB/go-libp2p-peer"
-
-	"github.com/ipfs/go-ipfs/core"
+	"gx/ipfs/QmTRhk7cgjUf2gfQ3p2M9KPECNZEW9XUrmHcFCgog4cPgB/go-libp2p-peer"
 )
 
-func Query(n *core.IpfsNode, peerID string) ([]peer.ID, error) {
-	dht, ok := n.Routing.(*routing.IpfsDHT)
-	if !ok {
-		return nil, errors.New("routing is not type IpfsDHT")
-	}
+func Query(dht *routing.IpfsDHT, peerID string) ([]peer.ID, error) {
 	id, err := peer.IDB58Decode(peerID)
 	if err != nil {
 		return nil, err
