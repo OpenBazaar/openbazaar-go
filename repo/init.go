b76244b45d898e724e7e9496a193fd7c17cec8cd
Chris Pacia <chris@ob1.io>
2019-03-12 16:54:13 -0400

    Create Migration020

--- repo/init.go
+++ repo/init.go
@@ -17,7 +17,7 @@ import (
 	"github.com/tyler-smith/go-bip39"
 )
 
-const RepoVersion = "20"
+const RepoVersion = "21"
 
 var log = logging.MustGetLogger("repo")
 var ErrRepoExists = errors.New("IPFS configuration file exists. Reinitializing would overwrite your keys. Use -f to force overwrite.")
