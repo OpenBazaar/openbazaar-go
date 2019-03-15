d738a0006a35784e31bf42c80e58039dd4fee5cf
Chris Pacia <chris@ob1.io>
2019-03-15 13:08:15 -0400

    Update error string

--- api/utils_test.go
+++ api/utils_test.go
@@ -10,16 +10,16 @@ func Test_extractModeratorChanges(t *testing.T) {
 
 	toAdd, toDelete := extractModeratorChanges(newModList, &currentModList)
 	if len(toAdd) != 1 {
-		t.Errorf("Test_extractModeratorChanges returned incorrect number of additions: expected %d got %d", 1, len(toAdd))
+		t.Errorf("Returned incorrect number of additions: expected %d got %d", 1, len(toAdd))
 	}
 	if len(toDelete) != 1 {
-		t.Errorf("Test_extractModeratorChanges returned incorrect number of deletions: expected %d got %d", 1, len(toDelete))
+		t.Errorf("Returned incorrect number of deletions: expected %d got %d", 1, len(toDelete))
 	}
 
 	if toAdd[0] != "x" {
-		t.Errorf("Test_extractModeratorChanges returned incorrect addition: expected a got %s", toAdd[0])
+		t.Errorf("Returned incorrect addition: expected a got %s", toAdd[0])
 	}
 	if toDelete[0] != "b" {
-		t.Errorf("Test_extractModeratorChanges returned incorrect deletion: expected b got %s", toDelete[0])
+		t.Errorf("Returned incorrect deletion: expected b got %s", toDelete[0])
 	}
 }
