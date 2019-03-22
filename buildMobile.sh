8181ad9fe4ceb76a7a6af4da34414570834ef199
Brian Hoffman <brianchoffman@gmail.com>
2019-03-22 08:10:43 -0400

    Update build script for mobile

--- buildMobile.sh
+++ buildMobile.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-# Build iOS framework
-make ios_framework
+case "$TRAVIS_OS_NAME" in
+  "linux")
+  ;;
 
-# Build Android framework
-make android_framework
+  "osx")
+
+    brew install ant
+    brew install maven
+    brew install gradle
+    brew install android-sdk
+    brew install android-ndk
+
+    android update sdk --no-ui
+
+    export ANT_HOME=/usr/local/opt/ant
+    export MAVEN_HOME=/usr/local/opt/maven
+    export GRADLE_HOME=/usr/local/opt/gradle
+    export ANDROID_HOME=/usr/local/opt/android-sdk
+    export ANDROID_NDK_HOME=/usr/local/opt/android-ndk
+
+    
+
+    # Build iOS framework
+    make ios_framework
+
+    # Build Android framework
+    make android_framework
+
+  ;;
+esac
