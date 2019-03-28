76928e7eeb60b0bf58ec4913c55e365b3ed73677
Brian Hoffman <brianchoffman@gmail.com>
2019-03-28 15:53:44 -0400

    Update build scripts

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+mkdir dist && cd dist/
+
 if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
-    TARGETS=${1:-darwin/amd64}
+#    TARGETS=${1:-darwin/amd64}
+    export GOBIN=$GOPATH/bin
+    go install openbazaard.go
+    mv $GOBIN/openbazaard dist/openbazaar-go-darwin-10.6-amd64
 else
     TARGETS=${1:-linux/386,linux/amd64,linux/arm}
+    export CGO_ENABLED=1
+    docker pull karalabe/xgo-latest
+    go get github.com/karalabe/xgo
+    xgo -go=1.10 --targets=$TARGETS ../
+    chmod +x *
 fi
-
-export CGO_ENABLED=1
-docker pull karalabe/xgo-latest
-go get github.com/karalabe/xgo
-mkdir dist && cd dist/
-xgo -go=1.10 --targets=$TARGETS ../
-chmod +x *
--- deploy.sh
+++ deploy.sh
@@ -8,23 +8,27 @@ if [ ! -z "$TRAVIS_TAG" ] &&
 
   cd dist
 
-  # Copy libwinpthread-1.dll (user must rename the dll for their system to libwinpthread-1.dll)
-  cp ../.travis/win32/libwinpthread-1.dll libwinpthread-1.win32.dll
-  cp ../.travis/win64/libwinpthread-1.dll libwinpthread-1.win64.dll
+  if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then
 
-  # Calculate SHA512 hashes
-  sha512sum * > sha512_checksums.txt
+      # Copy libwinpthread-1.dll (user must rename the dll for their system to libwinpthread-1.dll)
+      cp ../.travis/win32/libwinpthread-1.dll libwinpthread-1.win32.dll
+      cp ../.travis/win64/libwinpthread-1.dll libwinpthread-1.win64.dll
 
-  # Load signing key
-  cp ../.travis/sign.key.gpg /tmp
-  gpg --yes --batch --passphrase=$GPG_PASS /tmp/sign.key.gpg
-  gpg --allow-secret-key-import --import /tmp/sign.key.gpg
-  rm /tmp/sign.key.gpg
+      # Calculate SHA512 hashes
+      sha512sum * > sha512_checksums.txt
 
-  # Sign hash file
-  gpg --clearsign --digest-algo SHA512 --armor --output sha512_checksums.asc --passphrase=$GPG_PASS --default-key $GPG_KEYID sha512_checksums.txt
+      # Load signing key
+      cp ../.travis/sign.key.gpg /tmp
+      gpg --yes --batch --passphrase=$GPG_PASS /tmp/sign.key.gpg
+      gpg --allow-secret-key-import --import /tmp/sign.key.gpg
+      rm /tmp/sign.key.gpg
 
-  rm sha512_checksums.txt
+      # Sign hash file
+      gpg --clearsign --digest-algo SHA512 --armor --output sha512_checksums.asc --passphrase=$GPG_PASS --default-key $GPG_KEYID sha512_checksums.txt
+
+      rm sha512_checksums.txt
+
+  fi
 
   # Upload to GitHub Release page
   ghr --username OpenBazaar -t $GITHUB_TOKEN --replace --prerelease --debug $TRAVIS_TAG .
