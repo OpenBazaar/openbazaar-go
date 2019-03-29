01a0767bd82255a640d1a16a223fdfd1e02782d8
Brian Hoffman <brianchoffman@gmail.com>
2019-03-29 12:07:21 -0400

    Fix files

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-mkdir dist && cd dist/
+mkdir dist
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 #    TARGETS=${1:-darwin/amd64}
     export GOBIN=$GOPATH/bin
     go install openbazaard.go
     mv $GOBIN/openbazaard dist/openbazaar-go-darwin-10.6-amd64
+    cd dist/
 else
     TARGETS=${1:-linux/386,linux/amd64,linux/arm}
     export CGO_ENABLED=1
     docker pull karalabe/xgo-latest
     go get github.com/karalabe/xgo
+    cd dist/
     xgo -go=1.10 --targets=$TARGETS ../
     chmod +x *
 fi
