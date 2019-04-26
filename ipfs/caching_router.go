036d896a413d06ab76d75a82b19cc3c35e2b7945
Mike Greenberg <mikeg@ob1.io>
2019-04-26 14:30:06 -0400

    [#1557] Make CachingRouter implement routing.PubKeyFetcher

--- ipfs/caching_router.go
+++ ipfs/caching_router.go
@@ -5,8 +5,9 @@ import (
 	"encoding/hex"
 	"errors"
 
-	//routinghelpers "gx/ipfs/QmRCrPXk2oUwpK1Cj2FXrUotRpddUxz56setkny2gz13Cx/go-libp2p-routing-helpers"
 	dht "gx/ipfs/QmSY3nkMNLzh9GdbFKK5tT7YMfLpf52iUZ8ZRkr29MJaa5/go-libp2p-kad-dht"
+	ci "gx/ipfs/QmTW4SdgBWq9GjsBsHeUx8WuGxzhgzAf88UMH2w62PC8yK/go-libp2p-crypto"
+	peer "gx/ipfs/QmYVXrKrKHDC9FobgmcmshCDyWwdrfwfanNQN4oxJ9Fk3h/go-libp2p-peer"
 	routing "gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing"
 	ropts "gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing/options"
 	record "gx/ipfs/QmbeHtaBy9nZsW4cHRcvgVY4CnDhXudE2Dr6qDxS7yg9rX/go-libp2p-record"
@@ -72,6 +73,13 @@ func (r *CachingRouter) GetValue(ctx context.Context, key string, opts ...ropts.
 		log.Errorf("api cache put found dht value (%s): %s", hex.EncodeToString([]byte(key)), err.Error())
 	}
 	return val, nil
+}
+
+func (r *CachingRouter) GetPublicKey(ctx context.Context, p peer.ID) (ci.PubKey, error) {
+	if dht, ok := r.IpfsRouting.(routing.PubKeyFetcher); ok {
+		return dht.GetPublicKey(ctx, p)
+	}
+	return nil, routing.ErrNotSupported
 }
 
 func (r *CachingRouter) SearchValue(ctx context.Context, key string, opts ...ropts.Option) (<-chan []byte, error) {
