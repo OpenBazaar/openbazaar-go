435315b7cc7db5898a5f65559579a45d0eaf14d3
Chris Pacia <chris@ob1.io>
2019-04-29 11:11:10 -0400

    Clean up CachingRouter

--- ipfs/caching_router.go
+++ ipfs/caching_router.go
@@ -3,11 +3,10 @@ package ipfs
 import (
 	"context"
 	"errors"
-	routinghelpers "gx/ipfs/QmRCrPXk2oUwpK1Cj2FXrUotRpddUxz56setkny2gz13Cx/go-libp2p-routing-helpers"
-	dht "gx/ipfs/QmSY3nkMNLzh9GdbFKK5tT7YMfLpf52iUZ8ZRkr29MJaa5/go-libp2p-kad-dht"
-	routing "gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing"
-	ropts "gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing/options"
-	record "gx/ipfs/QmbeHtaBy9nZsW4cHRcvgVY4CnDhXudE2Dr6qDxS7yg9rX/go-libp2p-record"
+	"gx/ipfs/QmRCrPXk2oUwpK1Cj2FXrUotRpddUxz56setkny2gz13Cx/go-libp2p-routing-helpers"
+	"gx/ipfs/QmSY3nkMNLzh9GdbFKK5tT7YMfLpf52iUZ8ZRkr29MJaa5/go-libp2p-kad-dht"
+	"gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing"
+	"gx/ipfs/QmYxUdYY9S6yg5tSPVin5GFTvtfsLauVcr7reHDD3dM8xf/go-libp2p-routing/options"
 )
 
 var (
@@ -18,7 +17,6 @@ type CachingRouter struct {
 	apiRouter    *APIRouter
 	tieredRouter routinghelpers.Tiered
 	routing.IpfsRouting
-	RecordValidator record.Validator
 }
 
 func NewCachingRouter(dht *dht.IpfsDHT, apiRouter *APIRouter) *CachingRouter {
@@ -34,7 +32,6 @@ func NewCachingRouter(dht *dht.IpfsDHT, apiRouter *APIRouter) *CachingRouter {
 		apiRouter:       apiRouter,
 		tieredRouter:    tierd,
 		IpfsRouting:     dht,
-		RecordValidator: dht.Validator,
 	}
 }
 
@@ -57,7 +54,7 @@ func (r *CachingRouter) PutValue(ctx context.Context, key string, value []byte, 
 func (r *CachingRouter) GetValue(ctx context.Context, key string, opts ...ropts.Option) ([]byte, error) {
 	value, err := r.tieredRouter.GetValue(ctx, key, opts...)
 	if err == nil {
-		go r.IpfsRouting.PutValue(ctx, key, value, opts...)
+		go r.apiRouter.PutValue(ctx, key, value, opts...)
 	}
 	return value, err
 }
