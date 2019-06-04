a9de5171c95241a85ac0df380745805a0a8f3cac
Mike Greenberg <mikeg@ob1.io>
2019-06-03 22:13:50 -0400

    Remove logging to stdout in mobile package; Logging cleanup

--- mobile/config.go
+++ mobile/config.go
@@ -2,14 +2,7 @@ package mobile
 
 import (
 	"github.com/OpenBazaar/wallet-interface"
-	"github.com/op/go-logging"
 )
-
-var stdoutLogFormat = logging.MustStringFormatter(
-	`%{color:reset}%{color}%{time:15:04:05.000} [%{shortfunc}] [%{level}] %{message}`,
-)
-
-var logger logging.Backend
 
 // NodeConfig struct of the config parameters to be passed when creating a new node
 type NodeConfig struct {
