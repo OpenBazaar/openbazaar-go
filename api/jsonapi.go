d0fa04abcce671cf76c28c632e838e69ee12f7a7
Mike Greenberg <mikeg@ob1.io>
2019-06-11 18:12:27 -0400

    [#1169] Linter fixes caught by govet

--- api/jsonapi.go
+++ api/jsonapi.go
@@ -1478,7 +1478,7 @@ func (i *jsonAPIHandler) GETProfile(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		if profile.PeerID != peerID {
-			ErrorResponse(w, http.StatusNotFound, err.Error())
+			ErrorResponse(w, http.StatusNotFound, "invalid profile: peer id mismatch on found profile")
 			return
 		}
 		w.Header().Set("Cache-Control", "public, max-age=600, immutable")
