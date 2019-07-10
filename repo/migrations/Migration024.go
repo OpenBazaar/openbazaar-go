bcca56da7dafd6f3d2ff3c27362372f3ae5833d0
Ashwin Mangale <ashwin.mangale@gmail.com>
2019-07-10 21:48:45 +0530

    lint fix

--- repo/migrations/Migration024.go
+++ repo/migrations/Migration024.go
@@ -9,34 +9,34 @@ import (
 )
 
 const (
-	// MigrationCreateMessages_AM06MessagesCreateSQL the messages create sql
-	MigrationCreateMessages_AM06MessagesCreateSQL = "create table messages (messageID text primary key not null, orderID text, message_type integer, message blob, peerID text, url text, acknowledged bool, tries integer, created_at integer, updated_at integer);"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_MessageID the messages index on messageID sql
-	MigrationCreateMessages_AM06CreateIndexMessagesSQL_MessageID = "create index index_messages_messageID on messages (messageID);"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_OrderID_MType the messages composite index on orderID and messageType create sql
-	MigrationCreateMessages_AM06CreateIndexMessagesSQL_OrderID_MType = "create index index_messages_orderIDmType on messages (orderID, message_type);"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_PeerID_MType the messages composite index on peerID and messageType create sql
-	MigrationCreateMessages_AM06CreateIndexMessagesSQL_PeerID_MType = "create index index_messages_peerIDmType on messages (peerID, message_type);"
-	// MigrationCreateMessages_AM06MessagesDeleteSQL the messages delete sql
-	MigrationCreateMessages_AM06MessagesDeleteSQL = "drop table if exists messages;"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_MessageID delete the messages index on messageID sql
-	MigrationCreateMessages_AM06DeleteIndexMessagesSQL_MessageID = "drop index if exists index_messages_messageID;"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_OrderID_MType delete the messages composite index on orderID and messageType create sql
-	MigrationCreateMessages_AM06DeleteIndexMessagesSQL_OrderID_MType = "drop index if exists index_messages_orderIDmType;"
-	// MigrationCreateMessages_AM06CreateIndexMessagesSQL_PeerID_MType delete the messages composite index on peerID and messageType create sql
-	MigrationCreateMessages_AM06DeleteIndexMessagesSQL_PeerID_MType = "drop index if exists index_messages_peerIDmType;"
-	// MigrationCreateMessages_AM06UpVer set the repo Up version
-	MigrationCreateMessages_AM06UpVer = "25"
-	// MigrationCreateMessages_AM06DownVer set the repo Down version
-	MigrationCreateMessages_AM06DownVer = "24"
+	// MigrationCreateMessagesAM06MessagesCreateSQL the messages create sql
+	MigrationCreateMessagesAM06MessagesCreateSQL = "create table messages (messageID text primary key not null, orderID text, message_type integer, message blob, peerID text, url text, acknowledged bool, tries integer, created_at integer, updated_at integer);"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLMessageID the messages index on messageID sql
+	MigrationCreateMessagesAM06CreateIndexMessagesSQLMessageID = "create index index_messages_messageID on messages (messageID);"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLOrderIDMType the messages composite index on orderID and messageType create sql
+	MigrationCreateMessagesAM06CreateIndexMessagesSQLOrderIDMType = "create index index_messages_orderIDmType on messages (orderID, message_type);"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLPeerIDMType the messages composite index on peerID and messageType create sql
+	MigrationCreateMessagesAM06CreateIndexMessagesSQLPeerIDMType = "create index index_messages_peerIDmType on messages (peerID, message_type);"
+	// MigrationCreateMessagesAM06MessagesDeleteSQL the messages delete sql
+	MigrationCreateMessagesAM06MessagesDeleteSQL = "drop table if exists messages;"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLMessageID delete the messages index on messageID sql
+	MigrationCreateMessagesAM06DeleteIndexMessagesSQLMessageID = "drop index if exists index_messages_messageID;"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLOrderIDMType delete the messages composite index on orderID and messageType create sql
+	MigrationCreateMessagesAM06DeleteIndexMessagesSQLOrderIDMType = "drop index if exists index_messages_orderIDmType;"
+	// MigrationCreateMessagesAM06CreateIndexMessagesSQLPeerIDMType delete the messages composite index on peerID and messageType create sql
+	MigrationCreateMessagesAM06DeleteIndexMessagesSQLPeerIDMType = "drop index if exists index_messages_peerIDmType;"
+	// MigrationCreateMessagesAM06UpVer set the repo Up version
+	MigrationCreateMessagesAM06UpVer = "25"
+	// MigrationCreateMessagesAM06DownVer set the repo Down version
+	MigrationCreateMessagesAM06DownVer = "24"
 )
 
-// MigrationCreateMessages_AM06  local migration struct
-type MigrationCreateMessages_AM06 struct{}
+// MigrationCreateMessagesAM06  local migration struct
+type MigrationCreateMessagesAM06 struct{}
 
 // Migration024  migration struct
 type Migration024 struct {
-	MigrationCreateMessages_AM06
+	MigrationCreateMessagesAM06
 }
 
 func createMessages(repoPath, databasePassword, rVer string, testnetEnabled bool) error {
@@ -67,19 +67,19 @@ func createMessages(repoPath, databasePassword, rVer string, testnetEnabled bool
 	if err != nil {
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06MessagesCreateSQL); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06MessagesCreateSQL); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06CreateIndexMessagesSQL_MessageID); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06CreateIndexMessagesSQLMessageID); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06CreateIndexMessagesSQL_OrderID_MType); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06CreateIndexMessagesSQLOrderIDMType); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06CreateIndexMessagesSQL_PeerID_MType); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06CreateIndexMessagesSQLPeerIDMType); err != nil {
 		tx.Rollback()
 		return err
 	}
@@ -123,19 +123,19 @@ func deleteMessages(repoPath, databasePassword, rVer string, testnetEnabled bool
 	if err != nil {
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06DeleteIndexMessagesSQL_MessageID); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06DeleteIndexMessagesSQLMessageID); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06DeleteIndexMessagesSQL_OrderID_MType); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06DeleteIndexMessagesSQLOrderIDMType); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06DeleteIndexMessagesSQL_PeerID_MType); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06DeleteIndexMessagesSQLPeerIDMType); err != nil {
 		tx.Rollback()
 		return err
 	}
-	if _, err = tx.Exec(MigrationCreateMessages_AM06MessagesDeleteSQL); err != nil {
+	if _, err = tx.Exec(MigrationCreateMessagesAM06MessagesDeleteSQL); err != nil {
 		tx.Rollback()
 		return err
 	}
@@ -152,11 +152,11 @@ func deleteMessages(repoPath, databasePassword, rVer string, testnetEnabled bool
 }
 
 // Up the migration Up code
-func (MigrationCreateMessages_AM06) Up(repoPath, databasePassword string, testnetEnabled bool) error {
-	return createMessages(repoPath, databasePassword, MigrationCreateMessages_AM06UpVer, testnetEnabled)
+func (MigrationCreateMessagesAM06) Up(repoPath, databasePassword string, testnetEnabled bool) error {
+	return createMessages(repoPath, databasePassword, MigrationCreateMessagesAM06UpVer, testnetEnabled)
 }
 
 // Down the migration Down code
-func (MigrationCreateMessages_AM06) Down(repoPath, databasePassword string, testnetEnabled bool) error {
-	return deleteMessages(repoPath, databasePassword, MigrationCreateMessages_AM06DownVer, testnetEnabled)
+func (MigrationCreateMessagesAM06) Down(repoPath, databasePassword string, testnetEnabled bool) error {
+	return deleteMessages(repoPath, databasePassword, MigrationCreateMessagesAM06DownVer, testnetEnabled)
 }
