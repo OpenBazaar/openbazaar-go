0a9da612b8d53ab91384b1cc6f8edb4a2477e061
Mike Greenberg <mikeg@ob1.io>
2019-09-24 11:29:02 -0400

    Fix bug when attempting to unpin bad hashes

--- ipfs/pin.go
+++ ipfs/pin.go
@@ -2,30 +2,39 @@ package ipfs
 
 import (
 	"context"
-
-	"github.com/ipfs/go-ipfs/core/coreapi"
+	"fmt"
+	"strings"
 
 	coreiface "gx/ipfs/QmXLwxifxwfc2bAwq6rdjbYqAsGzWsDE9RM5TWMGtykyj6/interface-go-ipfs-core"
 	options "gx/ipfs/QmXLwxifxwfc2bAwq6rdjbYqAsGzWsDE9RM5TWMGtykyj6/interface-go-ipfs-core/options"
 
 	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
 )
 
-/* Recursively un-pin a directory given its hash.
-   This will allow it to be garbage collected. */
+// UnPinDir removes all content from the published root directory to be
+// garbage collected later by IPFS.
 func UnPinDir(n *core.IpfsNode, rootHash string) error {
+	// attempt to properly handle variations on rootHash
+	if !strings.HasPrefix(rootHash, "/ipfs/") {
+		rootHash = fmt.Sprintf("/ipfs/%s", rootHash)
+	}
+	if !strings.HasPrefix(rootHash, "/ipfs") && strings.HasPrefix(rootHash, "/") {
+		rootHash = fmt.Sprintf("/ipfs%s", rootHash)
+	}
+
 	api, err := coreapi.NewCoreAPI(n)
 	if err != nil {
-		return err
+		return fmt.Errorf("ipfs api: %s", err)
 	}
-	p, err := coreiface.ParsePath("/ipfs/" + rootHash)
+	p, err := coreiface.ParsePath(rootHash)
 	if err != nil {
-		return err
+		return fmt.Errorf("parsing ipfs path (%s): %s", rootHash, err)
 	}
 
 	rp, err := api.ResolvePath(context.Background(), p)
 	if err != nil {
-		return err
+		return fmt.Errorf("resolve path (%s): %s", p, err)
 	}
 
 	return api.Pin().Rm(context.Background(), rp, options.Pin.RmRecursive(true))
