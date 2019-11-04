7af6e7eeab5edb76831734dd7d26cb323795fff2
Brian Hoffman <brianchoffman@gmail.com>
2019-11-04 17:20:12 -0500

    Update shell script

--- build-osx.sh
+++ build-osx.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-ulimit -n 32000
+echo "Increase the maximum number of open file descriptors on macOS"
+NOFILE=20480
+sudo sysctl -w kern.maxfiles=$NOFILE
+sudo sysctl -w kern.maxfilesperproc=$NOFILE
+sudo launchctl limit maxfiles $NOFILE $NOFILE
+sudo launchctl limit maxfiles
+ulimit -S -n $NOFILE
+ulimit -n
 
 HOMEBREW_NO_AUTO_UPDATE=1
 brew update-reset
