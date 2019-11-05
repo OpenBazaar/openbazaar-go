25d6201e7a452d15acff700c56d36a2e71197d57
Brian Hoffman <brianchoffman@gmail.com>
2019-11-05 09:14:33 -0500

    Update shell script

--- build-osx.sh
+++ build-osx.sh
@@ -14,3 +14,4 @@ brew update-reset
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+
