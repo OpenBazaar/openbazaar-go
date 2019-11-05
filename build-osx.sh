14029518d5dbdc130b17da13ee5ea591a31fb274
Brian Hoffman <brianchoffman@gmail.com>
2019-11-05 13:28:21 -0500

    fix build

--- build-osx.sh
+++ build-osx.sh
@@ -5,7 +5,6 @@ brew update-reset
 brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
 sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
 sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
-
 brew services start docker-machine
 sudo spctl --master-disable
 brew cask install virtualbox
