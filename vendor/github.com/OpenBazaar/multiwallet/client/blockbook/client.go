9e831f6a8570b786d68f8faa00288304e2486138
Brian Hoffman <brianchoffman@gmail.com>
2019-12-05 15:07:59 -0500

    Fix

--- vendor/github.com/OpenBazaar/multiwallet/client/blockbook/client.go
+++ vendor/github.com/OpenBazaar/multiwallet/client/blockbook/client.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
-	//"net"
 	"net/http"
 	"net/url"
 	"path"
@@ -162,16 +161,20 @@ func NewBlockBookClient(apiUrl string, proxyDialer proxy.Dialer) (*BlockBookClie
 		return nil, err
 	}
 
-	//dial := net.Dial
-	//if proxyDialer != nil {
-	//	dial = proxyDialer.Dial
-	//}
+	var customClient http.Client
+	if proxyDialer != nil {
+		dial := proxyDialer.Dial
+		tbTransport := &http.Transport{Dial: dial}
+		customClient = http.Client{Timeout: time.Second * 30, Transport: tbTransport}
+	} else {
+		customClient = http.Client{Timeout: time.Second * 30}
+	}
 
 	bch := make(chan model.Block)
 	tch := make(chan model.Transaction)
-	//tbTransport := &http.Transport{Dial: dial}
+
 	ic := &BlockBookClient{
-		HTTPClient:      http.Client{Timeout: time.Second * 30},
+		HTTPClient:      customClient,
 		apiUrl:          u,
 		proxyDialer:     proxyDialer,
 		blockNotifyChan: bch,
