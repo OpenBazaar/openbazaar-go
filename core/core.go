f3f0b76f7367967918758de3fd96fbab219ba7c4
Chris <chris@ob1.io>
2020-05-06 14:45:30 -0400

    Bump version number

--- core/core.go
+++ core/core.go
@@ -32,7 +32,7 @@ import (
 
 const (
 	// VERSION - current version
-	VERSION = "0.14.1"
+	VERSION = "0.14.2"
 	// USERAGENT - user-agent header string
 	USERAGENT = "/openbazaar-go:" + VERSION + "/"
 )
