500b2bc84ecd8a894686217fd85c1365e8b519c5
Brian Hoffman <brianchoffman@gmail.com>
2020-06-11 14:15:22 -0400

    gofmt

--- repo/checkout.go
+++ repo/checkout.go
@@ -12,12 +12,12 @@ type CheckoutCurrency struct {
 }
 
 type CheckoutBreakdown struct {
-	BasePrice       string            `json:"basePrice"`
-	Coupon          string            `json:"coupon"`
-	OptionSurcharge string            `json:"optionSurcharge"`
-	PriceCurrency   CheckoutCurrency  `json:"priceCurrency"`
-	Quantity        string            `json:"quantity"`
-	ShippingPrice   string            `json:"shippingPrice"`
-	Tax             string            `json:"tax"`
-	TotalPrice      string            `json:"totalPrice"`
+	BasePrice       string           `json:"basePrice"`
+	Coupon          string           `json:"coupon"`
+	OptionSurcharge string           `json:"optionSurcharge"`
+	PriceCurrency   CheckoutCurrency `json:"priceCurrency"`
+	Quantity        string           `json:"quantity"`
+	ShippingPrice   string           `json:"shippingPrice"`
+	Tax             string           `json:"tax"`
+	TotalPrice      string           `json:"totalPrice"`
 }
