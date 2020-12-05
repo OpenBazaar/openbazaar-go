6cb1294fc91bdc9b510a4321183e6b02a889003a
Brian Hoffman <brianchoffman@gmail.com>
2020-12-05 08:23:38 -0500

    Bump verison

--- core/core.go
+++ core/core.go
@@ -32,7 +32,7 @@ import (
 
 const (
 	// VERSION - current version
-	VERSION = "0.14.5"
+	VERSION = "0.14.6"
 	// USERAGENT - user-agent header string
 	USERAGENT = "/openbazaar-go:" + VERSION + "/"
 )
